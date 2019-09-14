dd52c80afa896efbf72431ff334384f93d36310f
margotr <github@margotrutgers.nl>
2019-09-14 18:13:04 +0200

    Add Logged In student as message in navbar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -11,25 +11,29 @@ class Nav extends React.Component {
     }
 
     render() {
-        
         return (
             <div>
                 <div className="mx-auto" style={{ width: "200px" }}>
                     <FontAwesomeIcon size={"4x"} icon={faPalette} />
                     <h2 className="title">Atelier</h2>
                 </div>
-                <ul className="nav">
-                    {this.props.loggedIn ?
-                        <React.Fragment>
-                            <li className="nav-item"><Link className="nav-link" to="/roleview">Submissions</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/logout" onClick={this.props.onLogout}>Logout</Link></li>
-                        </React.Fragment> :
-                        <React.Fragment>
-                            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-                        </React.Fragment>
-                    }
-                </ul>
+                <div className="nav-bar">
+                    <ul className="nav menu-left">
+                        {this.props.loggedIn ?
+                            <React.Fragment>
+                                <li className="nav-item"><Link className="nav-link" to="/roleview">Submissions</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/logout" onClick={this.props.onLogout}>Logout</Link></li>
+                            </React.Fragment> :
+                            <React.Fragment>
+                                <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                            </React.Fragment>
+                        }
+                    </ul>
+                    <div className="menu-right" id="user">
+                        Logged in as {this.props.role}: {this.props.email}
+                    </div>
+                </div>
             </div>
         )
     }
