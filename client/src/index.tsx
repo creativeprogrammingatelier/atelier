0156bd12c999759e59f26950d72006f1948d7d7d
Andrew Heath <a.j.heath@student.utwente.nl>
2019-12-11 10:40:41 +0100

    Changes to test cli

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-//Changes to commit
+//Changes to commit changes more
 ReactDOM.render((
     <BrowserRouter>
         <App />
