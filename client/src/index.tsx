d80a96a9550f98ab45453be644e2169e80743a49
Andrew Heath <a.j.heath@student.utwente.nl>
2019-12-11 11:05:36 +0100

    small change to test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-//Changes to commit changes more 12
+//Changes to commit changes more 123
 ReactDOM.render((
     <BrowserRouter>
         <App />
--- deploy_prod.bash
+++ deploy_prod.bash
@@ -3,14 +3,12 @@ echo "Deploying to Atelier to Production from master"
 
 pm2 stop npm 
 cd /var/www/linux571.ewi.utwente.nl/atelier
-echo ‘post-receive: git check out…’
+echo "Updating repo from git"
 git --git-dir=/var/www/linux571.ewi.utwente.nl/atelier.git --work-tree=/var/www/linux571.ewi.utwente.nl/atelier checkout master -f
-
-
-
+echo "NPM install"
 npm i 
-npm run build-all
-pm2 start npm  
+npm run prod-build
+npm run prod-server
 echo "Atelier Deployed"
 
 
