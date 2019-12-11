db46f6e6e2f605bd7318527af7b523ac5ce3a0e8
Andrew Heath <a.j.heath@student.utwente.nl>
2019-12-11 11:02:48 +0100

    Small changes to  check commit

--- deploy_prod.bash
+++ deploy_prod.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Deploying to Atelier to Production from master"
 
-cd #/var/www/linx571.ewi.utwente.nl/atelier-core 
 pm2 stop npm 
-git checkout master
-git pull 
+cd /var/www/linux571.ewi.utwente.nl/atelier
+echo ‘post-receive: git check out…’
+git --git-dir=/var/www/linux571.ewi.utwente.nl/atelier.git --work-tree=/var/www/linux571.ewi.utwente.nl/atelier checkout master -f
+
+
+
 npm i 
 npm run build-all
 pm2 start npm  
