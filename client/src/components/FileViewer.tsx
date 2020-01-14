d55f371613250b8bd03e7053ad628f37d6ec1af3
margotr <github@margotrutgers.nl>
2020-01-14 10:19:19 +0100

    Better navbar

--- client/src/components/FileViewer.tsx
+++ client/src/components/FileViewer.tsx
@@ -16,39 +16,48 @@ class FileViewer extends React.Component<{ files: IFile[], update: Function }> {
             viewedFile: null,
             currentLineNumber: 0,
         }
-        this.codeViewerRef = React.createRef<CodeViewer>();    
+        this.codeViewerRef = React.createRef<CodeViewer>();
     }
 
     populateTable = () => {
         let rows = [];
         if (this.props.files && this.props.files[0] != undefined) {
-
             for (let file of this.props.files) {
-                rows.push(<tr key={file._id} className={this.state.viewedFile != null && this.state.viewedFile.id != null &&  this.state.viewedFile.id == file._id ? 'table-active': ""} >
+                rows.push(<tr key={file._id} className={this.state.viewedFile != null && this.state.viewedFile.id != null && this.state.viewedFile.id == file._id ? 'table-active' : ""} >
                     <td>{file.name}</td>
-                    <td><button key={`download-${file._id}`} onClick={() => FileHelper.downloadFile(file._id, ()=>{alert("Failed to download file")})}><FontAwesomeIcon icon={faFileDownload} /></button></td>
-                    <td><button key={`view-${file._id}`} onClick={() => FileHelper.getFile(file._id, (file: IFile) => { this.setState({ viewedFile: file })}, () => alert("Failed to get file"))}><FontAwesomeIcon icon={faEye} /></button></td>
-                    <td><button key={`view-${file._id}`} onClick={() => FileHelper.deleteFile(file._id, () => this.props.update(), ()=> alert('File deletion has failed'))}><FontAwesomeIcon icon={faTrash} /></button></td>
+                    <td><button key={`download-${file._id}`} onClick={() => FileHelper.downloadFile(file._id, () => { alert("Failed to download file") })}><FontAwesomeIcon icon={faFileDownload} /></button></td>
+                    <td><button key={`view-${file._id}`} onClick={() => FileHelper.getFile(file._id, (file: IFile) => { this.setState({ viewedFile: file }) }, () => alert("Failed to get file"))}><FontAwesomeIcon icon={faEye} /></button></td>
+                    <td><button key={`view-${file._id}`} onClick={() => FileHelper.deleteFile(file._id, () => this.props.update(), () => alert('File deletion has failed'))}><FontAwesomeIcon icon={faTrash} /></button></td>
                 </tr>);
             }
         }
+
+        if (this.props.files.length > 0) {
+            return (
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr key='header'>
+                                <th scope="col">File Name</th>
+                                <th scope="col">Download</th>
+                                <th scope="col" >View</th>
+                                <th scope="col" >Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {(rows) ? rows : null}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+
         return (
             <div>
-                <table className="table">
-                    <thead>
-                        <tr key='header'>
-                            <th scope="col">File Name</th>
-                            <th scope="col">Download</th>
-                            <th scope="col" >View</th>
-                            <th scope="col" >Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(rows) ? rows : null}
-                    </tbody>
-                </table>
+                No files were uploaded
             </div>
         )
+
     }
 
     updateCurrentLineNumber = (lineNumber: number) => {
@@ -66,10 +75,10 @@ class FileViewer extends React.Component<{ files: IFile[], update: Function }> {
                 <div>
                     <div className="row">
                         <div className="col-sm-8">
-                            {(this.state.viewedFile != null) ? <CodeViewer {  ...{file: this.state.viewedFile, commentLineNumber: this.state.currentLineNumber, updateLineNumber: this.updateCurrentLineNumber  }} /> : null}
+                            {(this.state.viewedFile != null) ? <CodeViewer {...{ file: this.state.viewedFile, commentLineNumber: this.state.currentLineNumber, updateLineNumber: this.updateCurrentLineNumber }} /> : null}
                         </div>
                         <div className="col-sm-4">
-                            {(this.state.viewedFile != null ) ? <CommentsViewer {...{updateCurrentLineNumber: this.updateCurrentLineNumber, currentLineNumber: this.state.currentLineNumber, file: this.state.viewedFile }} /> : null}
+                            {(this.state.viewedFile != null) ? <CommentsViewer {...{ updateCurrentLineNumber: this.updateCurrentLineNumber, currentLineNumber: this.state.currentLineNumber, file: this.state.viewedFile }} /> : null}
                         </div>
 
                     </div>
