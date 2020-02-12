71c493e0138bbfc62e95ec84b4ec43d2b55581fa
Arthur Rump <hello@arthurrump.com>
2020-02-12 14:46:51 +0100

    Make DataTable more flexible

--- client/src/components/general/DataTable.tsx
+++ client/src/components/general/DataTable.tsx
@@ -3,27 +3,28 @@ import React from 'react';
 interface DataTableProperties<T> {
     /** Title to show above the table */
     title: string,
-    /** Defines the table head, as a pair of the display 
-     *  title and the property in the data 
-     */
-    head: Array<[string, string]>,
     /** List of objects to display in the table */
-    data: T[]
+    data: T[],
+    /** List of columns in the table as a pair of the 
+     *  title of that column and a function to render the
+     *  item as a React element
+     */
+    table: Array<[string, (item: T) => (JSX.Element | string)]>
 }
 
 /** A simple table, but probably providing more table-fucntionality
  *  in the future
  */
-export function DataTable<T>({ title, head, data }: DataTableProperties<T>) {
+export function DataTable<T>({ title, data, table }: DataTableProperties<T>) {
     return (
         <div>
             <span>{title}</span>
             <table>
-                <thead><tr>{head.map(([text, _]) => <th>{text}</th>)}</tr></thead>
+                <thead><tr>{table.map(([header, _]) => <th>{header}</th>)}</tr></thead>
                 <tbody>
                     {data.map(item =>
                         <tr>
-                            {head.map(([_, prop]) => <td>{item[prop]}</td>)}
+                            {table.map(([_, f]) => <td>{f(item)}</td>)}
                         </tr>)}
                 </tbody>
             </table>
