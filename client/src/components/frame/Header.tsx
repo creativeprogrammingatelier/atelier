d90d6660bcf53d21cf492108cc916cb6f5621e35
Cas <c.t.sievers@student.utwente.nl>
2020-02-18 10:40:03 +0100

    Properly aligned right header button

--- client/src/components/frame/Header.tsx
+++ client/src/components/frame/Header.tsx
@@ -23,7 +23,7 @@ export function Header({ title, transparent, fixed, leftButton, rightButton}: He
 			<Navbar className={"row no-gutters" + (transparent && " bg-transparent") + (fixed && " position-fixed")}>
 				{(leftButton || rightButton) && <div className="col-2 text-left">{leftButton && <HeaderButton name={leftButton.name} icon={leftButton.icon} onClick={leftButton.click}/>}</div>}
 				{(leftButton || rightButton) && <div className="col-8 text-center">{title && <h3>{title}</h3>}</div>}
-				{(leftButton || rightButton) && <div className="col-2 text-right">{rightButton && <HeaderButton  name={rightButton.name} icon={rightButton.icon} onClick={rightButton.click}/>}</div>}
+				{(leftButton || rightButton) && <div className="col-2 text-right">{rightButton && <HeaderButton name={rightButton.name} icon={rightButton.icon} onClick={rightButton.click} right/>}</div>}
 				{!(leftButton || rightButton) && <div className="col-12 text-center"><h3>{title}</h3></div>}
 			</Navbar>
 		</div>
