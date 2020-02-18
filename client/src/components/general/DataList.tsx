47c96075c5f1b7bf1274f6b0746847f80fe8182e
Cas <c.t.sievers@student.utwente.nl>
2020-02-18 10:28:50 +0100

    Added t seconds/minutes/hours/days/weeks/months/years ago for the data lists

--- client/src/components/general/DataList.tsx
+++ client/src/components/general/DataList.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import {DataBlock, DataBlockProperties} from './DataBlock';
+import React, {useEffect, useState} from 'react';
+import {DataBlock, DataTag} from './DataBlock';
 import {Header} from '../frame/Header';
 
+interface DataListEntryProperties {
+	title: string,
+	text?: string,
+	time: Date,
+	tags?: DataTag[]
+}
 interface DataListProperties {
 	header: string,
-	list: DataBlockProperties[]
+	list: DataListEntryProperties[]
 }
 export function DataList({header, list}: DataListProperties) {
+	const [currentTime, setCurrentTime] = useState(new Date());
+
+	useEffect(() => {
+		setInterval(() => {
+			setCurrentTime(new Date())
+		}, 1000)
+	}, []);
+
 	return <div>
 		<Header title={header}/>
 		<div className="pt-2">
-			{list.map((block) => <DataBlock title={block.title} text={block.text} time={block.time} tags={block.tags}/>)}
+			{list.map((block) => <DataBlock title={block.title} text={block.text} time={{start:block.time, offset:currentTime}} tags={block.tags}/>)}
 		</div>
 	</div>
 }
