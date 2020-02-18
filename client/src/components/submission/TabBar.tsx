988b12dbec411e584d38fb86bc2911d18cc599f0
Cas <c.t.sievers@student.utwente.nl>
2020-02-18 13:59:11 +0100

    Mid-progress in tab bar creation

--- client/src/components/submission/TabBar.tsx
+++ client/src/components/submission/TabBar.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import {TabButton} from "./TabButton";
+import {TabButton} from './TabButton';
+import {Nav} from 'react-bootstrap';
 
-export function TabBar(props : any) {
-    return (
-        <div>
-            <TabButton
-                value= ""
-                name = "Project"
-                onClick = {props.onClick}
-            />
-            <TabButton
-                value = "code"
-                name = "Code"
-                onClick = {props.onClick}
-            />
-            <TabButton
-                value = "comments"
-                name = "Comments"
-                onClick = {props.onClick}
-            />
-            <TabButton
-                value = "share"
-                name = "Share"
-                onClick = {props.onClick}
-            />
-        </div>
-    )
+interface TabProperties {
+	id: string,
+	icon: JSX.Element,
+	text: string,
+	location: string
+}
+interface TabBarProperties {
+	tabs: TabProperties[],
+	active: string
+}
+export function TabBar({active, tabs}: TabBarProperties) {
+	console.log("Rendering some tabs");
+	console.log(tabs);
+	console.log(active);
+	for (const tab of tabs) {
+		console.log(tab);
+		console.log(tab.id);
+		console.log(tab.id === active);
+	}
+	return <div className="tabBar">
+		<Nav fill justify variant="pills">
+			{tabs.map((tab) => <TabButton icon={tab.icon} text={tab.text} location={tab.location} active={tab.id === active}/>)}
+		</Nav>
+	</div>
 }
