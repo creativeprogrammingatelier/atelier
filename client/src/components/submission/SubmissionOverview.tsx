2e44f18d5e9b7875d31f9df2895c91b83f63e2aa
Cas <c.t.sievers@student.utwente.nl>
2020-02-18 14:27:33 +0100

    Completed-ish tab bar

--- client/src/components/submission/SubmissionOverview.tsx
+++ client/src/components/submission/SubmissionOverview.tsx
@@ -6,54 +6,72 @@ import {ShareTab} from './ShareTab';
 import {TabBar} from './TabBar';
 import {ProjectTab} from "./ProjectTab";
 import {Frame} from '../frame/Frame';
+import {FiCode, FiFolder, FiMessageSquare, FiShare2} from 'react-icons/all';
 
 interface SubmissionOverviewProps {
-	match: any,
-	history: any
+	match: {
+		params: {
+			tab: string,
+			fileId: string
+		}
+	}
 }
 
-function getTab(token: string | undefined) {
-	console.log("Finding what tab we're rendering");
-	console.log(token);
-	if (token === undefined || token === "") return 'project';
+function getParameter(token: string | undefined) {
+	if (token === undefined || token === "") return "project";
 	return token.toLowerCase();
 }
 
-export function SubmissionOverview({match, history} : SubmissionOverviewProps) {
-	const [tab, setTab] = useState(getTab(match.params['tab'])); // Get tab from match object
-	const [file, setFile] = useState(getTab(match.params['fileId'])); // Keep track of code being viewed
-
-	// Handle events to the tab buttons here
-	function handleTabChange(event : any) {
-		const {value} = event.target;
-
-		const tab = getTab(value);
-		setTab(tab);
-
-		history.push(`/submission/1/${value}`);
-	}
+export function SubmissionOverview({match} : SubmissionOverviewProps) {
+	const [tab, setTab] = useState(getParameter(match.params.tab)); // Get tab from match object
+	const [file, setFile] = useState(getParameter(match.params.fileId)); // Keep track of code being viewed
 
 	function changeFile(file : string) {
 		setFile(file);
-		setTab('code');
+		setTab("code");
 	}
 
 	// Display certain tab
 	let currentTab = <div><h1>Tab not found!</h1></div>;
-	if (tab == 'code') {
+	if (tab === "code") {
 		currentTab =  <CodeTab fileName={file}/>
-	} else if (tab == 'comments') {
+	} else if (tab === "comments") {
 		currentTab =  <CommentTab />
-	} else if (tab == 'share') {
+	} else if (tab === "share") {
 		currentTab = <ShareTab />
-	} else if (tab == 'project') {
+	} else if (tab === "project") {
 		currentTab = <ProjectTab setFile = {changeFile}/>
 	}
 
+	const submissionId = "1";
+	const submissionPath = "/submission/"+submissionId;
+
 	return (
 		<Frame title="Submission" user={{id:"1", name:"John Doe"}} sidebar search={"/submission/../search"}>
-			<TabBar onClick={handleTabChange}/>
-			<hr/>
+			<TabBar
+				tabs={[{
+					id: "project",
+					icon: <FiFolder size={28} color="#FFFFFF"/>,
+					text: "Files",
+					location: submissionPath
+				}, {
+					id: "code",
+					icon: <FiCode size={28} color="#FFFFFF"/>,
+					text: "Code",
+					location: submissionPath+"/code"
+				}, {
+					id: "comments",
+					icon: <FiMessageSquare size={28} color="#FFFFFF"/>,
+					text: "Comments",
+					location: submissionPath+"/comments"
+				}, {
+					id: "share",
+					icon: <FiShare2 size={28} color="#FFFFFF"/>,
+					text: "Share",
+					location: submissionPath+"/share"
+				}]}
+				active={tab}
+			/>
 			{currentTab}
 		</Frame>
 	)
--- client/src/components/submission/TabBar.tsx
+++ client/src/components/submission/TabBar.tsx
@@ -13,17 +13,7 @@ interface TabBarProperties {
 	active: string
 }
 export function TabBar({active, tabs}: TabBarProperties) {
-	console.log("Rendering some tabs");
-	console.log(tabs);
-	console.log(active);
-	for (const tab of tabs) {
-		console.log(tab);
-		console.log(tab.id);
-		console.log(tab.id === active);
-	}
-	return <div className="tabBar">
-		<Nav fill justify variant="pills">
+	return <Nav fill justify variant="pills" className="fixed-bottom">
 			{tabs.map((tab) => <TabButton icon={tab.icon} text={tab.text} location={tab.location} active={tab.id === active}/>)}
 		</Nav>
-	</div>
 }
