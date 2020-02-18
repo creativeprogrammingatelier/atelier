ab41bccd4c3fcbbd7341eb9016ea7e80c0d21ec5
Cas <c.t.sievers@student.utwente.nl>
2020-02-18 10:44:31 +0100

    Header buttons no longer have a name

--- client/src/components/frame/Header.tsx
+++ client/src/components/frame/Header.tsx
@@ -7,12 +7,10 @@ interface HeaderProperties {
 	transparent?: boolean,
 	fixed?: boolean,
 	leftButton?: {
-		name: string,
 		icon: JSX.Element,
 		click: React.MouseEventHandler
 	},
 	rightButton?: {
-		name: string,
 		icon: JSX.Element,
 		click: React.MouseEventHandler
 	}
@@ -21,9 +19,9 @@ export function Header({ title, transparent, fixed, leftButton, rightButton}: He
 	return (
 		<div className="fluid-container">
 			<Navbar className={"row no-gutters" + (transparent && " bg-transparent") + (fixed && " position-fixed")}>
-				{(leftButton || rightButton) && <div className="col-2 text-left">{leftButton && <HeaderButton name={leftButton.name} icon={leftButton.icon} onClick={leftButton.click}/>}</div>}
+				{(leftButton || rightButton) && <div className="col-2 text-left">{leftButton && <HeaderButton icon={leftButton.icon} onClick={leftButton.click}/>}</div>}
 				{(leftButton || rightButton) && <div className="col-8 text-center">{title && <h3>{title}</h3>}</div>}
-				{(leftButton || rightButton) && <div className="col-2 text-right">{rightButton && <HeaderButton name={rightButton.name} icon={rightButton.icon} onClick={rightButton.click} right/>}</div>}
+				{(leftButton || rightButton) && <div className="col-2 text-right">{rightButton && <HeaderButton icon={rightButton.icon} onClick={rightButton.click} right/>}</div>}
 				{!(leftButton || rightButton) && <div className="col-12 text-center"><h3>{title}</h3></div>}
 			</Navbar>
 		</div>
