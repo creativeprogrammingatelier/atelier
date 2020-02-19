82c417f74a3bf2d50f305f2ea9c4f52c8a81df9d
Arthur Rump <hello@arthurrump.com>
2020-02-19 13:54:20 +0100

    "Fix" error in SocketMiddleware

--- api/src/middleware/SocketMiddleware.ts
+++ api/src/middleware/SocketMiddleware.ts
@@ -1,15 +1,17 @@
 import {Request} from 'express';
 import {Socket} from 'socket.io';
-import {IComment} from '../../../models/comment';
+// import {Comment} from '../../../models/Comment';
+// TODO
+type Comment = any;
 
 export default class SocketMiddleware {
 
-	static sendCommentUpdate(request: Request, fileId: string, comment: IComment): void {
+	static sendCommentUpdate(request: Request, fileId: string, comment: Comment): void {
 		const io: Socket = request.app.settings['socket-io'];
 		io.emit(fileId, {type: 'put', comment: comment});
 	}
 
-	static sendCommentDelete(request: Request, fileId: string, comment: IComment): void {
+	static sendCommentDelete(request: Request, fileId: string, comment: Comment): void {
 		const io: Socket = request.app.settings['socket-io'];
 		io.emit(fileId, {type: 'delete', comment: comment});
 	}
