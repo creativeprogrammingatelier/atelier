8c3a94e471d7468af071e6499736ffde4fad0b39
Cas <c.t.sievers@student.utwente.nl>
2020-02-21 10:11:16 +0100

    IDs are now strings, tabs, and code style

--- api/src/routes/CourseRouter.ts
+++ api/src/routes/CourseRouter.ts
@@ -2,38 +2,37 @@
  * Api routes relating to a course
  */
 
-import express, { Response, Request } from 'express';
+import express, {Response, Request} from "express";
 import CoursesHelper from "../database/CoursesHelper";
 import {Course} from "../../../models/course";
 
 export const courseRouter = express.Router();
 
 const courseSubmissions = {
-    submissions: [
-        {
-            user: "John Doe",
-            name: "Uploaded helpitbroke.zip",
-            time: new Date(),
-            tags: [{name : "help", color : "red", dark: true}, {name : "me", color : "red", dark: true}, {name : "now", color : "red", dark: true}]},
-        {
-            user: "John Doe",
-            name: "Uploaded Project 'ImDaBest'",
-            time: new Date(2020, 1, 17, 15).toLocaleString(),
-            tags: [{name : "fuck", color : "green", dark: true}, {name : "yeah", color : "green", dark: true}]
-        },
-        {
-            user: "Mary Doe",
-            name: "Uploaded project 'ImmaDropOutNow",
-            time: new Date(2020, 0, 9 , 15).toLocaleString(),
-            tags: [{name : "fuck", color : "orange"}, {name : "off", color : "orange"}]
-        }
-    ]
+	submissions: [
+		{
+			user: "John Doe",
+			name: "Uploaded helpitbroke.zip",
+			time: new Date(),
+			tags: [{name: "help", color: "red", dark: true}, {name: "me", color: "red", dark: true}, {name: "now", color: "red", dark: true}]
+		}, {
+			user: "John Doe",
+			name: "Uploaded Project 'ImDaBest'",
+			time: new Date(2020, 1, 17, 15).toLocaleString(),
+			tags: [{name: "fuck", color: "green", dark: true}, {name: "yeah", color: "green", dark: true}]
+		}, {
+			user: "Mary Doe",
+			name: "Uploaded project 'ImmaDropOutNow",
+			time: new Date(2020, 0, 9, 15).toLocaleString(),
+			tags: [{name: "fuck", color: "orange"}, {name: "off", color: "orange"}]
+		}
+	]
 };
 
 /**
  * /api/course/:courseId/submissions
  */
-courseRouter.get('/:courseId/submissions',
-    (request: Request, result: Response) => {
-        result.send(courseSubmissions);
-    });
+courseRouter.get("/:courseId/submissions",
+	(request: Request, result: Response) => {
+		result.send(courseSubmissions);
+	});
