2bc264aa69becd8adea04c62ffbf800abb07f85b
Cas <c.t.sievers@student.utwente.nl>
2020-02-21 09:55:56 +0100

    IDs are now strings, tabs, and code style

--- api/src/routes/UserRouter.ts
+++ api/src/routes/UserRouter.ts
@@ -5,89 +5,89 @@
  *  - list of user submissions
  */
 
-import express, { Response, Request } from 'express';
+import express, {Response, Request} from "express";
 
 export const userRouter = express.Router();
 
 const userResponse = {
-    user : {
-        userId : 1,
-        name : "John Doe"
-    },
-    submissions : [
-        {
-            name : "Project 1",
-            submissionId : 1,
-            user : "Jarik",
-            userId : 1,
-            date : new Date(),
-            state : "open",
-            comments : [{
-                commentId : 1,
-                commentThreadId : 1,
-                userId : 1,
-                author : "Jarik",
-                date : new Date(),
-                text : "Comment Text"
-            }, {
-                commentId : 2,
-                commentThreadId : 1,
-                userId : 1,
-                author : "Jarik",
-                date : new Date(),
-                text : "Comment Text 2"
-            }],
-            files : [{
-                name : "main.pde",
-                fileId : 1
-            }, {
-                name : "main2.pde",
-                fileId : 2
-            }]
-        }, {
-            name : "Project 2",
-            submissionId: 2,
-            user : "Jarik",
-            userId : 1,
-            date : new Date(),
-            state : "open",
-            comments : [{
-                commentId : 1,
-                commentThreadId : 1,
-                userId : 1,
-                author : "Jarik",
-                date : new Date(),
-                text : "Comment Text 3"
-            }, {
-                commentId : 2,
-                commentThreadId : 1,
-                userId : 1,
-                author : "Jarik",
-                date : new Date(),
-                text : "Comment Text 4"
-            }],
-            files : [{
-                name : "main3.pde",
-                fileId : 3
-            }, {
-                name : "main4.pde",
-                fileId : 4
-            }]
-        }],
-    commentThreads : [{
-        commentThreadId : 1,
-        name : "comment thread 1",
-        comments : [{
-            name : "comment 1",
-            author : "Jarik",
-            text : "comment text",
-            userId : 1
-        }]
-    }]
+	user: {
+		userId: "1",
+		name: "John Doe"
+	},
+	submissions: [
+		{
+			name: "Project 1",
+			submissionId: "1",
+			user: "Jarik",
+			userId: "1",
+			date: new Date(),
+			state: "open",
+			comments: [{
+				commentId: "1",
+				commentThreadId: "1",
+				userId: "1",
+				author: "Jarik",
+				date: new Date(),
+				text: "Comment Text"
+			}, {
+				commentId: "2",
+				commentThreadId: "1",
+				userId: "1",
+				author: "Jarik",
+				date: new Date(),
+				text: "Comment Text 2"
+			}],
+			files: [{
+				name: "main.pde",
+				fileId: "1"
+			}, {
+				name: "main2.pde",
+				fileId: "2"
+			}]
+		}, {
+			name: "Project 2",
+			submissionId: "2",
+			user: "Jarik",
+			userId: "1",
+			date: new Date(),
+			state: "open",
+			comments: [{
+				commentId: "1",
+				commentThreadId: "1",
+				userId: "1",
+				author: "Jarik",
+				date: new Date(),
+				text: "Comment Text 3"
+			}, {
+				commentId: "2",
+				commentThreadId: "1",
+				userId: "1",
+				author: "Jarik",
+				date: new Date(),
+				text: "Comment Text 4"
+			}],
+			files: [{
+				name: "main3.pde",
+				fileId: "3"
+			}, {
+				name: "main4.pde",
+				fileId: "4"
+			}]
+		}],
+	commentThreads: [{
+		commentThreadId: "1",
+		name: "comment thread 1",
+		comments: [{
+			name: "comment 1",
+			author: "Jarik",
+			text: "comment text",
+			userId: "1"
+		}]
+	}]
 };
 
 
-userRouter.get('/:userId/submissions',
-    (request: Request, result: Response) => {
-        result.send(userResponse);
-    });
+userRouter.get("/:userId/submissions",
+	(request: Request, result: Response) => {
+		result.send(userResponse);
+	});
