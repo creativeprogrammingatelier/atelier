9d32fd2cfa8802c0d93cafddb56f22cb504676f9
Cas <c.t.sievers@student.utwente.nl>
2020-02-24 09:09:16 +0100

    I changed stuff in master?? In untracked files?!

---
+++ api/src/database/SnippetDB.ts
@@ -0,0 +1,82 @@
+import {query, extract, map, one} from "./HelperDB";
+import {Snippet, DBSnippet, convertSnippet} from '../../../models/Snippet';
+
+/**
+ * 
+ * @Author Rens Leendertz
+ */
+export class SnippetDB {
+	static getAllSnippets(){
+		return SnippetDB.filterSnippet({})
+	}
+	
+	static getSnippetsByFile(fileID : string){
+		return SnippetDB.filterSnippet({fileID})
+	}
+	
+	static getSnippetByID(snippetID : string) {
+		return SnippetDB.filterSnippet({snippetID}).then(one)
+	}
+
+	static filterSnippet(snippet : Snippet){
+		const {
+			snippetID = undefined,
+			lineStart = undefined,
+			lineEnd = undefined,
+			charStart = undefined,
+			charEnd = undefined,
+			fileID = undefined
+		} = snippet
+		return query(`SELECT * FROM "Snippets" 
+			WHERE
+				($1::uuid IS NULL OR snippetID=$1)
+			AND ($2::integer IS NULL OR lineStart=$2)
+			AND ($3::integer IS NULL OR lineEnd=$3)
+			AND ($4::integer IS NULL OR charStart=$4)
+			AND ($5::integer IS NULL OR charEnd=$5)
+			AND ($6::uuid IS NULL OR fileID=$6)
+			`,[snippetID, lineStart, lineEnd, charStart, charEnd, fileID])
+		.then(extract).then(map(convertSnippet))
+	}
+
+	static addSnippet(snippet : Snippet){
+		const {
+			lineStart,
+			lineEnd,
+			charStart,
+			charEnd,
+			fileID
+		} = snippet
+		return query(`INSERT INTO "Snippets" 
+			VALUES (DEFAULT, $1, $2, $3, $4, $5) 
+			RETURNING *`, [lineStart, lineEnd, charStart, charEnd,fileID])
+		.then(extract).then(map(convertSnippet)).then(one)
+	}
+
+	static updateSnippet(snippet : Snippet){
+		const {
+			snippetID,
+			lineStart = undefined,
+			lineEnd = undefined,
+			charStart = undefined,
+			charEnd = undefined,
+			fileID = undefined
+		} = snippet
+		return query(`UPDATE "Snippets" SET 
+			lineStart = COALESCE($2, lineStart),
+			lineEnd = COALESCE($3, lineEnd),
+			charStart = COALESCE($4, charStart),
+			charEnd = COALESCE($5, charEnd),
+			fileID = COALESCE($6, fileID)
+			WHERE snippetID=$1
+			RETURNING *`, [snippetID, lineStart, lineEnd, charStart, charEnd, fileID])
+		.then(extract).then(map(convertSnippet)).then(one)
+	}
+
+	static deleteSnippet(snippetID : string){
+		return query(`DELETE FROM "Snippets" 
+			WHERE snippetID = $1 
+			RETURNING *`, [snippetID])
+		.then(extract).then(map(convertSnippet)).then(one)
+	}
+}
---
+++ api/src/database/UserDB.ts
@@ -0,0 +1,151 @@
+import {query, extract, map, one, searchify} from "./HelperDB";
+import {User, DBUser, convertUser} from '../../../models/User';
+import bcrypt from 'bcrypt';
+import { Pool } from "pg";
+
+/**
+ * Users middleware provides helper methods for interacting with users in the DB
+ * userID, name, role, email
+ * @Author Rens Leendertz
+ */
+
+export class UserDB {
+	/**
+	 * calls onSuccess() with all known users that have the global role 'user', except password hash
+	 */
+	static getAllStudents() {
+		return query(`SELECT userID, name, globalRole, email 
+			FROM "Users" 
+			WHERE globalRole = 'user'`)
+			.then(extract).then(map(convertUser))
+	}
+
+	/**
+	 * calls onSuccess() with all users in the system.
+	 */
+	static getAllUsers() {
+		return query(`SELECT userID, name, globalRole, email FROM "Users"`)
+			.then(extract).then(map(convertUser))
+	}
+
+	/**
+	 * calls onSuccess() with a student based on its userID, without password hash
+	 */
+	static getUserByID(userID : string) {
+		return query(`SELECT userID, name, globalRole, email 
+			FROM "Users" where userID = $1`, [userID])
+			.then(extract).then(map(convertUser)).then(one)
+	}
+
+	static searchUser(searchString : string, limit? : number){
+		if (limit === undefined || limit < 0) limit=undefined
+		searchString = searchify(searchString)
+		return query(`SELECT userID, name, globalRole, email
+			FROM "Users"
+			WHERE name ILIKE $1
+			LIMIT $2`, [searchString, limit])
+			.then(extract).then(map(convertUser))
+	}
+
+	/**
+	 * creates a user based on the @param user. 
+	 * All fields but userID are required
+	 * if a userID is present, it will be ignored.
+	 */
+	static createUser(user : User) {
+		const {
+			email,
+			password,
+			role,
+			name
+		} = user;
+		const hash = password === undefined ? undefined : UserDB.hashPassword(password);
+		return query(`INSERT INTO "Users" 
+			VALUES (DEFAULT, $1, $2, $3, $4) 
+			RETURNING *`, [name, role, email, password])
+			.then(extract).then(map(convertUser)).then(one)
+	}
+	/**
+	 * update a user using the @param user.
+	 * userID is required to identify the user.
+	 * all other fields may or may not be present and will be updated accordingly.
+	 */
+	static updateUser(user: User) {
+		const {
+			userID, //primary key is required
+			email = undefined,
+			password = undefined,
+			role = undefined,
+			name = undefined
+		} = user
+		const hash = password === undefined ? undefined : UserDB.hashPassword(password)
+		return query(`UPDATE "Users"
+			SET 
+			email = COALESCE($2, email),
+			hash = COALESCE($3, hash),
+			globalRole = COALESCE($4, globalRole),
+			name = COALESCE($5, name)
+			WHERE userID = $1
+			RETURNING * `, [userID, email, hash, role, name])
+			.then(extract).then(map(convertUser)).then(one)
+	}
+
+	/**
+	 * deletes a user from the database, based on the userID.
+	 */
+	static deleteUser(userID: string) {
+		return query(`DELETE FROM "Users" 
+			WHERE userID = $1 
+			RETURNING *`, [userID])
+			.then(extract).then(map(convertUser)).then(one)
+	}
+
+	/**
+	 * Checks if a (user, password) combination exists in the database.
+	 * this requires parameters 'email' and 'password'
+	 * onSuccess will be called with the corresponding userID to proceed with login.
+	 */
+	static async loginUser(
+		loginRequest : {email:string, password:string}, 
+		onSuccess: (userID : string) => void, 
+		onUnauthorised: () => void, 
+		onFailure : (error : Error) => void) {
+		const {
+			email,
+			password
+		} = loginRequest;
+		const res = await query<DBUser, [string]>(`SELECT * 
+			FROM "Users" 
+			WHERE email = $1`, [email])
+			.then(extract).then(one).catch((error : Error) => {
+				console.error(error)
+				return onFailure(error)
+			})
+		if (res === undefined){
+			return onUnauthorised()
+		}
+		if (!res.hash){
+			return onFailure(Error('WTF is the database doing'))
+		}
+		const {hash, userid}= res
+		if (userid===undefined){
+			return onFailure(Error("the database is fking with us"))
+		}
+		if (UserDB.comparePassword(hash, password)){
+			return onSuccess(userid)
+		} else {
+			return onUnauthorised()
+		}
+	}
+
+	/**
+	 * 2 private methods to handle password hashing and comparing.
+	 */
+	private static comparePassword(hash: string, plain: string): boolean {
+		return bcrypt.compareSync(plain, hash);
+	}
+	private static hashPassword(plain: string): string {
+		const saltRounds=10;
+		return bcrypt.hashSync(plain, saltRounds);
+	}
+}
