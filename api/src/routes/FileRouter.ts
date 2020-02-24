538c9a3c239684d5741c5a904d882bf11e30fe57
Jarik <jarikgerbertkarsten@gmail.com>
2020-02-24 13:21:44 +0100

    dunno

--- api/src/routes/FileRouter.ts
+++ api/src/routes/FileRouter.ts
@@ -5,7 +5,7 @@
  *  - text (no json)
  */
 
-import express, { Response, Request } from 'express';
+import express, {Response, Request} from 'express';
 import {File} from "../../../models/File";
 import {FileDB} from "../database/FileDB";
 
@@ -13,21 +13,21 @@ const fs = require('fs');
 export const fileRouter = express.Router();
 
 fileRouter.get('/:fileID',
-    async (request : Request, result : Response) => {
+    async (request: Request, result: Response) => {
         const fileID = request.params.fileID;
 
-        const file : File = await FileDB.getFileByID(fileID);
+        const file: File = await FileDB.getFileByID(fileID);
 
         try {
-                // Get path for file, and load it as string
-                const pathname = "./uploads/" + file.pathname! + '.' + file.type!;
-                const fileBody = fs.readFileSync(pathname).toString();
+            // Get path for file, and load it as string
+            const pathname = "./uploads/" + file.pathname! + '.' + file.type!;
+            const fileBody = fs.readFileSync(pathname).toString();
 
-                result.send({
-                        ...file,
-                        body : fileBody
-                })
+            result.send({
+                ...file,
+                body: fileBody
+            })
         } catch (e) {
-                result.status(404).send({error:'file not found'})
+            result.status(404).send({error: 'file not found'})
         }
-});
+    });
