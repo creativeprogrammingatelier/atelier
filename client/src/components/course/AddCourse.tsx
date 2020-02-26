b5bc51e25eb9b920d0f4c5b9438dc4ec6cde17a2
Arthur Rump <hello@arthurrump.com>
2020-02-26 10:30:52 +0100

    Replace fetch in AddCourse

--- client/src/components/course/AddCourse.tsx
+++ client/src/components/course/AddCourse.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import {InputField} from "../general/InputField";
-import {Course} from "../../../../models/database/Course";
-import AuthHelper from './../../../helpers/AuthHelper';
+import React from 'react';
+import {InputField} from '../general/InputField';
+import {Course} from '../../../../models/database/Course';
+import { Fetch } from '../../../helpers/FetchHelper';
 
 interface AddCourseProps {
     handleResponse? : (course : Course) => void
@@ -9,21 +9,20 @@ interface AddCourseProps {
 
 export function AddCourse({handleResponse} : AddCourseProps) {
     // Create course
-    function createCourse(courseName : string) {
-        // TODO pass token so backend can get userID
-        AuthHelper.fetch('/api/course', {
-            method : 'POST',
-            body : JSON.stringify({
-                name : courseName,
-                state : 'open',
-            })
-        })
-            .then(response => response.json())
-            .then((course : Course) => {
-                if (handleResponse) {
-                    handleResponse(course);
-                }
+    async function createCourse(courseName : string) {
+        try {
+            const course = await Fetch.fetchJson<Course>('/api/course', {
+                method : 'POST',
+                body : JSON.stringify({
+                    name : courseName,
+                    state : 'open',
+                })
             });
+            handleResponse(course);
+        } catch (err) {
+            // TODO: handle error for the user
+            console.log(err);
+        }
     }
 
     return (
