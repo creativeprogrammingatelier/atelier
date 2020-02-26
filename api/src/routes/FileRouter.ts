20e816132d4bb14bba31fbedaf96cbfd94d7b82b
Arthur Rump <hello@arthurrump.com>
2020-02-26 14:29:49 +0100

    Add /file/:id/download

--- api/src/routes/FileRouter.ts
+++ api/src/routes/FileRouter.ts
@@ -6,8 +6,9 @@
  */
 
 import express from 'express';
+import path from 'path';
 import { AuthMiddleware } from '../middleware/AuthMiddleware';
-import {FileDB} from "../database/FileDB";
+import { FileDB } from "../database/FileDB";
 import { readFile } from '../helpers/FilesystemHelper';
 
 export const fileRouter = express.Router();
@@ -16,19 +17,21 @@ export const fileRouter = express.Router();
 fileRouter.use(AuthMiddleware.requireAuth);
 
 fileRouter.get('/:fileID', async (request, response) => {
-    const fileID = request.params.fileID;
-    
+    const fileID = request.params.fileID; 
     try {
         const file = await FileDB.getFileByID(fileID);
-        response.status(200).send(file);
-    } catch (e) {
-        response.status(404).send({error:'file not found'})
+        if (file !== undefined) {
+            response.status(200).send(file);
+        } else {
+            response.status(404).send({ error: "notfound", message: "The file you requested does not exist." });
+        }
+    } catch (err) {
+        response.status(500).send(); // This is not guaranteed to be JSON, so no error
     }
 });
 
 fileRouter.get('/:fileID/body', async (request, response) => {
     const fileID = request.params.fileID;
-    
     try {
         const file = await FileDB.getFileByID(fileID);
         const fileBody = await readFile(file.pathname!);
@@ -37,3 +40,17 @@ fileRouter.get('/:fileID/body', async (request, response) => {
         response.status(500).send(); // This is not guaranteed to be JSON, so no error
     }
 });
+
+fileRouter.get('/:fileID/download', async (request, response) => {
+    const fileID = request.params.fileID;
+    try {
+        const file = await FileDB.getFileByID(fileID);
+        const fileBody = await readFile(file.pathname!);
+        response.status(200)
+            .set('Content-Type', file.type!)
+            .set('Content-Disposition', `attachment; filename="${path.basename(file.pathname!)}"`)
+            .send(fileBody);
+    } catch (err) {
+        response.status(500).send(); // This is not guaranteed to be JSON, so no error
+    }
+})
