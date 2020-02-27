b7d08164f6482cdb7c00c3fd7d7a40888f879b06
Arthur Rump <hello@arthurrump.com>
2020-02-27 11:06:44 +0100

    Make API ordering in APIHelpers more logical

--- client/helpers/APIHelper.ts
+++ client/helpers/APIHelper.ts
@@ -9,6 +9,46 @@ import { Comment } from "../../models/database/Comment";
 
 // TODO: Fix all anys to be the correct model
 
+// Courses
+export const getCourses = () => 
+    Fetch.fetchJson<Course[]>('/api/course/');
+
+export const getCourse = (courseID: string) => 
+    Fetch.fetchJson<Course>(`/api/course/${courseID}`);
+    
+export const createCourse = (course: Course) => 
+    Fetch.fetchJson<Course>('/api/course', {
+        method : 'POST',
+        body : JSON.stringify(course)
+    });
+
+// Users
+export const getCurrentUser = () => 
+    Fetch.fetchJson<User>('/api/user/');
+
+export const getUser = (userId: string) => 
+    Fetch.fetchJson<User>(`/api/user/${userId}`);
+
+// Submissions
+export const getCourseSubmissions = (courseId: string) => 
+    Fetch.fetchJson<Submission[]>(`/api/submission/course/${courseId}`);
+    
+export const getUserSubmissions = (userId: string) => 
+    Fetch.fetchJson<Submission[]>(`/api/submission/user/${userId}`);
+
+export const getSubmission = (submissionID: string) => 
+    Fetch.fetchJson<Submission>(`/api/submission/${submissionID}`);
+
+// Files
+export const getFiles = (submissionID: string) => 
+    Fetch.fetchJson<File[]>(`/api/file/submission/${submissionID}`);
+
+export const getFile = (fileId: string) => 
+    Fetch.fetchJson<File>(`/api/file/${fileId}`);
+
+export const getFileContents = (fileId : string) => 
+    Fetch.fetchString(`/api/file/${fileId}/body`);
+    
 // CommentThreads
 export const getFileComments = (fileID: string) => 
     Fetch.fetchJson<ExtendedThread[]>(`/api/commentThread/file/${fileID}`);
@@ -31,46 +71,6 @@ export const createComment = (commentThreadID: string, comment: Comment) =>
         body : JSON.stringify(comment)
     });
 
-// Courses
-export const getCourses = () => 
-    Fetch.fetchJson<Course[]>('/api/course/');
-
-export const getCourse = (courseID: string) => 
-    Fetch.fetchJson<Course>(`/api/course/${courseID}`);
-    
-export const createCourse = (course: Course) => 
-    Fetch.fetchJson<Course>('/api/course', {
-        method : 'POST',
-        body : JSON.stringify(course)
-    });
-
-// Submissions
-export const getCourseSubmissions = (courseId: string) => 
-    Fetch.fetchJson<Submission[]>(`/api/submission/course/${courseId}`);
-    
-export const getUserSubmissions = (userId: string) => 
-    Fetch.fetchJson<Submission[]>(`/api/submission/user/${userId}`);
-
-export const getSubmission = (submissionID: string) => 
-    Fetch.fetchJson<Submission>(`/api/submission/${submissionID}`);
-
-// Files
-export const getFiles = (submissionID: string) => 
-    Fetch.fetchJson<File[]>(`/api/file/submission/${submissionID}`);
-
-export const getFile = (fileId: string) => 
-    Fetch.fetchJson<File>(`/api/file/${fileId}`);
-
-export const getFileContents = (fileId : string) => 
-    Fetch.fetchString(`/api/file/${fileId}/body`);
-
-// Users
-export const getCurrentUser = () => 
-    Fetch.fetchJson<User>('/api/user/');
-
-export const getUser = (userId: string) => 
-    Fetch.fetchJson<User>(`/api/user/${userId}`);
-    
 // Search
 export const search = (term: string) =>
     Fetch.fetchJson<any>(`/api/search?q=${term}`);
