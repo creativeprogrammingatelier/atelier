f673d25cbeb91f641d290c1d0168d99254440e5e
Arthur Rump <hello@arthurrump.com>
2020-02-27 09:21:13 +0100

    Add error handling to FileRouter

--- api/src/routes/FileRouter.ts
+++ api/src/routes/FileRouter.ts
@@ -3,9 +3,9 @@
 import express from 'express';
 import path from 'path'
 import { AuthMiddleware } from '../middleware/AuthMiddleware';
-import { FileDB } from "../database/FileDB";
+import { FileDB } from '../database/FileDB';
 import { readFile } from '../helpers/FilesystemHelper';
-import { handleError } from '../helpers/ErrorHelper';
+import { capture } from '../helpers/ErrorHelper';
 
 export const fileRouter = express.Router();
 
@@ -13,68 +13,40 @@ export const fileRouter = express.Router();
 fileRouter.use(AuthMiddleware.requireAuth);
 
 /** Get information of type `File` about a file */
-fileRouter.get('/:fileID', async (request, response, next) => {
+fileRouter.get('/:fileID', capture(async (request, response) => {
     const fileID = request.params.fileID;
-    try {
-        const file = await FileDB.getFileByID(fileID);
-        if (file !== undefined) {
-            response.status(200).send(file);
-        } else {
-            response.status(404).send({ error: "notfound", message: "The file you requested does not exist." });
-        }
-    } catch (err) {
-        handleError(err, next);
-    }
-});
+    const file = await FileDB.getFileByID(fileID);
+    response.status(200).send(file);
+}));
 
 /** Get a list of files related to a submission */
-fileRouter.get('/submission/:submissionID', async (request, response, next) => {
+fileRouter.get('/submission/:submissionID', capture(async (request, response) => {
     const submissionID: string = request.params.submissionID;
-    try {
-        const files = await FileDB.getFilesBySubmission(submissionID);
-        response.status(200).send(files);
-    } catch (err) {
-        handleError(err, next);
-    }
-});
+    const files = await FileDB.getFilesBySubmission(submissionID);
+    response.status(200).send(files);
+}));
 
 /** 
  * Get the contents of a file as the body of the response,
  * the Content-Type will be equivalent to the type of the file
  */
-fileRouter.get('/:fileID/body', async (request, response, next) => {
+fileRouter.get('/:fileID/body', capture(async (request, response) => {
     const fileID = request.params.fileID;
-    try {
-        const file = await FileDB.getFileByID(fileID);
-        if (file !== undefined) {
-            const fileBody = await readFile(file.pathname!);
-            response.status(200).set('Content-Type', file.type!).send(fileBody);
-        } else {
-            response.status(404).send({ error: "notfound", message: "The file you requested does not exist." });
-        }
-    } catch (err) {
-        handleError(err, next);
-    }
-});
+    const file = await FileDB.getFileByID(fileID);
+    const fileBody = await readFile(file.pathname!);
+    response.status(200).set("Content-Type", file.type!).send(fileBody);
+}));
 
 /**
  * Similar to /body, but with the Content-Disposition set to attachment,
  * so the browser will know to present a Save dialog
  */
-fileRouter.get('/:fileID/download', async (request, response, next) => {
+fileRouter.get('/:fileID/download', capture(async (request, response) => {
     const fileID = request.params.fileID;
-    try {
-        const file = await FileDB.getFileByID(fileID);
-        if (file !== undefined) {
-            const fileBody = await readFile(file.pathname!);
-            response.status(200)
-                .set('Content-Type', file.type!)
-                .set('Content-Disposition', `attachment; filename="${path.basename(file.pathname!)}"`)
-                .send(fileBody);
-        } else {
-            response.status(404).send({ error: "notfound", message: "The file you requested does not exist." });
-        }
-    } catch (err) {
-        handleError(err, next);
-    }
-})
+    const file = await FileDB.getFileByID(fileID);
+    const fileBody = await readFile(file.pathname!);
+    response.status(200)
+        .set("Content-Type", file.type!)
+        .set("Content-Disposition", `attachment; filename="${path.basename(file.pathname!)}"`)
+        .send(fileBody);
+}));
