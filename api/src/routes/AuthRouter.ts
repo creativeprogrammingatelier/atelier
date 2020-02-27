f10791ea6e39452ebcd91db9efdaf0699a3db0b7
Arthur Rump <hello@arthurrump.com>
2020-02-27 11:41:01 +0100

    Add error handling to AuthRouter

--- api/src/routes/AuthRouter.ts
+++ api/src/routes/AuthRouter.ts
@@ -5,43 +5,49 @@
  */
 import express from 'express';
 import { AuthMiddleware } from '../middleware/AuthMiddleware';
-import { issueToken, getCurrentUserID } from '../helpers/AuthenticationHelper';
+import { issueToken, getCurrentUserID, AuthError } from '../helpers/AuthenticationHelper';
 import { UserDB } from '../database/UserDB';
+import { capture } from '../helpers/ErrorHelper';
 
 export const authRouter = express.Router();
 
 /** Login endpoint, returns a token to use for subsequent requests */
-authRouter.post('/login', async (req, res, next) => {
+authRouter.post('/login', capture(async (request, response) => {
     UserDB.loginUser(
-        req.body,
+        request.body,
         // Success
         userID => {
             const token = issueToken(userID);
-            res.status(200).json({ token });
+            response.status(200).json({ token });
         },
         // Unauthorized
-        () => res.status(401).send(),
+        () => {
+            throw new AuthError("credentials.invalid", "Your email or password is incorrect.");
+        },
         // Error
-        err => next(err));
-});
+        err => { 
+            throw err; 
+        }
+    );
+}));
 
 /** Registration endpoint for new users */
-authRouter.post('/register', async (req, res) => {
+authRouter.post('/register', capture(async (request, response) => {
     try {
-        const user = await UserDB.createUser(req.body);
+        const user = await UserDB.createUser(request.body);
         const token = issueToken(user.userID!);
-        res.status(200).json({ token });
+        response.status(200).json({ token });
     } catch (err) {
-        res.status(500).json(err);
+        response.status(500).json(err);
     }
-});
+}));
 
 /** Refresh the current users authentication with a new token */
-authRouter.get('/refresh', AuthMiddleware.requireAuth, async (req, res) => {
-    const userID = await getCurrentUserID(req);
+authRouter.get('/refresh', AuthMiddleware.requireAuth, capture(async (request, response) => {
+    const userID = await getCurrentUserID(request);
     const token = issueToken(userID);
-    res.status(200).json({ token });
-});
+    response.status(200).json({ token });
+}));
 
 /** Checks if request JWT token passed is valid using withAuth middleware method */
 authRouter.get('/token', AuthMiddleware.requireAuth, (_, res) => res.sendStatus(200));
