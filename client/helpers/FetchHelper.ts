3ab1161b011bd0f404b201d131bfe45a903a2fae
Cas <c.t.sievers@student.utwente.nl>
2020-03-04 11:38:46 +0100

    Basic cache implementation

--- client/helpers/FetchHelper.ts
+++ client/helpers/FetchHelper.ts
@@ -1,102 +1,116 @@
-import AuthHelper from './AuthHelper';
-import { ServerError } from './../../models/api/ServerError';
+import AuthHelper from "./AuthHelper";
+import {ServerError} from "../../models/api/ServerError";
+import {cache} from "./FetchCache";
 
 export class Fetch {
-    /** 
-     * Combine headers in the options with separate headers into a single options object.
-     * In the case of duplicate headers, preference is given to those in the options.
-     */
-    private static combineHeaders(options: RequestInit, headers: HeadersInit) {
-        options.headers = 
-            options.headers 
-            ? { ...headers, ...options.headers } 
-            : headers;
-        return options;
-    }
+	/**
+	 * Combine headers in the options with separate headers into a single options object.
+	 * In the case of duplicate headers, preference is given to those in the options.
+	 */
+	private static combineHeaders(options: RequestInit, headers: HeadersInit) {
+		options.headers =
+			options.headers
+				? {...headers, ...options.headers}
+				: headers;
+		return options;
+	}
 
-    /** 
-     * Generic fetch, requesting JSON content from the server, 
-     * using the authentication token if the user is logged in.
-     * Throws a `FetchError` if the request was not succesful.
-     */
-    static async fetch(url: RequestInfo, options: RequestInit = {}) {
-        const headers: HeadersInit = {};
-        if (AuthHelper.loggedIn()) {
-            const token = AuthHelper.getToken();
-            if (token) headers["Authorization"] = token;
-        }
+	/**
+	 * Generic fetch, requesting JSON content from the server,
+	 * using the authentication token if the user is logged in.
+	 * Throws a `FetchError` if the request was not succesful.
+	 */
+	static async fetch<T>(url: RequestInfo, options: RequestInit = {}, handler?: (response: Response) => T) {
+		const headers: HeadersInit = {};
+		if (AuthHelper.loggedIn()) {
+			const token = AuthHelper.getToken();
+			if (token) headers["Authorization"] = token;
+		}
 
-        const res = await fetch(url, Fetch.combineHeaders(options, headers));
+		const doCache: boolean = options.method === undefined || options.method === "GET";
 
-        if (res.ok) {
-            return res;
-        } else {
-            throw new FetchError(res);
-        }
-    }
+		if (doCache && cache.has(url)) {
+			return cache.get(url);
+		}
 
-    /** 
-     * Do a fetch and convert the result into a JSON object. 
-     * Throws a `JsonFetchError` if the request was not succesful.
-     */
-    static async fetchJson<T>(url: RequestInfo, options: RequestInit = {}): Promise<T> {
-        const headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        };
+		const response = await fetch(url, Fetch.combineHeaders(options, headers));
 
-        try {
-            const res = await Fetch.fetch(url, Fetch.combineHeaders(options, headers));
-            return res.json();
-        } catch (err) {
-            if (err instanceof FetchError) {
-                throw await JsonFetchError.create(err.response);
-            } else {
-                throw err;
-            }
-        }
-    }
+		if (response.ok) {
+			if (handler) {
+				const data = handler(response);
+				if (doCache) {
+					cache.register(url, data);
+				}
+				return data;
+			}
+			if (doCache) {
+				cache.register(url, response);
+			}
+			return response;
+		} else {
+			throw new FetchError(response);
+		}
+	}
 
-    /** Do a fetch of binary data, returning the blob */
-    static async fetchBlob(url: RequestInfo, options: RequestInit = {}) {
-        const res = await Fetch.fetch(url, options);
-        return res.blob();
-    }
+	/**
+	 * Do a fetch and convert the result into a JSON object.
+	 * Throws a `JsonFetchError` if the request was not succesful.
+	 */
+	static async fetchJson<T>(url: RequestInfo, options: RequestInit = {}): Promise<T> {
+		const headers = {
+			"Accept": "application/json",
+			"Content-Type": "application/json"
+		};
 
-    /** Do a fetch for textual data and return the content as a string */
-    static async fetchString(url: RequestInfo, options: RequestInit = {}) {
-        const headers = {
-            // TODO: find out what is send to the server as the type of .pde files
-            "Accept": "text/*"
-        }
+		try {
+			return await Fetch.fetch(url, Fetch.combineHeaders(options, headers), (data) => data.json());
+		} catch (error) {
+			if (error instanceof FetchError) {
+				throw await JsonFetchError.create(error.response);
+			} else {
+				throw error;
+			}
+		}
+	}
 
-        const res = await Fetch.fetch(url, Fetch.combineHeaders(options, headers));
-        return res.text();
-    }
+	/** Do a fetch of binary data, returning the blob */
+	static async fetchBlob(url: RequestInfo, options: RequestInit = {}) {
+		return Fetch.fetch(url, options, (data) => data.blob());
+	}
+
+	/** Do a fetch for textual data and return the content as a string */
+	static async fetchString(url: RequestInfo, options: RequestInit = {}) {
+		const headers = {
+			// TODO: find out what is send to the server as the type of .pde files
+			"Accept": "text/*"
+		};
+
+		return Fetch.fetch(url, Fetch.combineHeaders(options, headers), (data) => data.text());
+	}
 }
 
 /** Error thrown on an unsuccesful fetch */
 export class FetchError extends Error {
-    /** The response sent by the server */
-    response: Response
+	/** The response sent by the server */
+	response: Response;
 
-    constructor(response: Response, message = `${response.status}: ${response.statusText}`) {
-        super(message);
-        this.response = response;
-    }
+	constructor(response: Response, message = `${response.status}: ${response.statusText}`) {
+		super(message);
+		this.response = response;
+	}
 }
 
 /** Error thrown on an unsuccesful fetch when requesting json data */
 export class JsonFetchError extends FetchError {
-    error: string
-    
-    private constructor(response: Response, err: ServerError) {
-        super(response, err.message);
-        this.error = err.error;
-    }
+	error: string;
 
-    static async create(response: Response) {
-        const err: ServerError = await response.json();
-        return new JsonFetchError(response, err);
-    }
+	private constructor(response: Response, err: ServerError) {
+		super(response, err.message);
+		this.error = err.error;
+	}
+
+	static async create(response: Response) {
+		const err: ServerError = await response.json();
+		return new JsonFetchError(response, err);
+	}
 }
