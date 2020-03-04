dfb4399b0830519360d9d02dd2036898a289081e
Cas <c.t.sievers@student.utwente.nl>
2020-03-04 14:44:12 +0100

    Fixing merge issues

--- client/src/components/course/CourseOverview.tsx
+++ client/src/components/course/CourseOverview.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
-import {Frame} from '../frame/Frame';
-import {DataBlockList} from '../general/data/DataBlockList';
-import {Loading} from '../general/loading/Loading';
+import {Frame} from "../frame/Frame";
+import {DataBlockList} from "../general/data/DataBlockList";
+import {Loading} from "../general/loading/Loading";
 import {Submission} from "../../../../models/database/Submission";
-import { getCourseSubmissions } from '../../../helpers/APIHelper';
-import { Uploader } from '../uploader/Uploader';
+import {getCourseSubmissions} from "../../../helpers/APIHelper";
+import {Uploader} from "../uploader/Uploader";
 import {Jumbotron} from "react-bootstrap";
 
 interface CourseOverviewProps {
@@ -16,7 +16,8 @@ interface CourseOverviewProps {
 }
 
 export function CourseOverview({match}: CourseOverviewProps) {
-	const [ reload, updateReload ] = useState(0);
+	const [reload, updateReload] = useState(0);
+	
 	return (
 		<Frame title="Course" sidebar search={"/course/../search"}>
 			<Jumbotron>
@@ -25,27 +26,26 @@ export function CourseOverview({match}: CourseOverviewProps) {
 			</Jumbotron>
 			<Uploader
 				courseId={match.params.courseId}
-				onUploadComplete={() => updateReload(rel => rel + 1)}
-			/>
+				onUploadComplete={() => updateReload(rel => rel + 1)}/>
 			<Loading<Submission[]>
-                loader={getCourseSubmissions}
-                params={[match.params.courseId]}
-                component={submissions =>
-                    <DataBlockList
-                        header="Submissions"
-                        list={submissions.map(submission => {
-                            console.log(submission);
-                            return {
-                                transport: `/submission/${submission.submissionID}`,
-                                title: submission.name === undefined ? "" : submission.name,
-                                text: submission.name === undefined ? "" : submission.name,
-                                time: submission.date === undefined ? new Date() : new Date(submission.date),
-                                tags : []
-                                //tags: submission.tags
-                            };
-                        })}
-                    />
-                }
+				loader={(courseId, reload) => getCourseSubmissions(courseId)}
+				params={[match.params.courseId, reload]}
+				component={submissions =>
+					<DataBlockList
+						header="Submissions"
+						list={submissions.map(submission => {
+							console.log(submission);
+							return {
+								transport: `/submission/${submission.submissionID}`,
+								title: submission.name === undefined ? "" : submission.name,
+								text: submission.name === undefined ? "" : submission.name,
+								time: submission.date === undefined ? new Date() : new Date(submission.date),
+								tags: []
+								//tags: submission.tags
+							};
+						})}
+					/>
+				}
 			/>
 		</Frame>
 	);
