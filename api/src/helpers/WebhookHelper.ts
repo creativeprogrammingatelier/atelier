7e84efdecdcac74ddf173fbf0912f6e1160871f5
Arthur Rump <hello@arthurrump.com>
2020-03-06 19:04:54 +0100

    Add tests for webhook integrations

--- api/src/helpers/WebhookHelper.ts
+++ api/src/helpers/WebhookHelper.ts
@@ -1,4 +1,5 @@
 import { PluginConfiguration, config } from './ConfigurationHelper';
+import fetch, { Request } from 'node-fetch';
 import crypto from 'crypto';
 
 interface WebhookRequest<T> {
@@ -6,22 +7,26 @@ interface WebhookRequest<T> {
     payload: T
 }
 
+export function getSubscribedPlugins(event: string) {
+    return config.plugins
+        .filter(plugin => plugin.hooks.includes(event));
+}
+
 export function raiseWebhookEvent<T>(event: string, body: T) {
     return Promise.all(
-        config.plugins
-            .filter(plugin => plugin.hooks.includes(event))
-            .map(plugin => postWebhook(plugin, event, body))
+        getSubscribedPlugins(event).map(plugin => postWebhook(plugin, event, body))
     );
 }
 
-async function postWebhook<T>(plugin: PluginConfiguration, event: string, body: T) {
+export function createWebhookRequest<T>(plugin: PluginConfiguration, event: string, body: T) {
     const request: WebhookRequest<T> = {
         event,
         payload: body
     }
     const payload = JSON.stringify(request);
-    const signature = crypto.createHmac('sha1', plugin.webhookSecret).update(payload).digest('base64');
-    const res = await fetch(plugin.webhookUrl, {
+    const signature = crypto.createHmac("sha1", plugin.webhookSecret).update(payload).digest("base64");
+    return new Request(plugin.webhookUrl, {
+        method: "POST",
         body: payload,
         headers: { 
             "Content-Type": "application/json",
@@ -29,6 +34,10 @@ async function postWebhook<T>(plugin: PluginConfiguration, event: string, body: 
             "X-Atelier-Signature": signature
         }
     });
+}
+
+async function postWebhook<T>(plugin: PluginConfiguration, event: string, body: T) {
+    const res = await fetch(createWebhookRequest(plugin, event, body));
     if (!res.ok) {
         // TODO: store this somewhere the plugin owner can see it
         console.log(`Error while posting webhook request. Recipient: ${plugin} Event: ${event}`);
