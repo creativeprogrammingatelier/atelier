d64595a45b343bb160d6f7bef043d233fee70c04
Jarik <jarikgerbertkarsten@gmail.com>
2020-03-11 21:42:37 +0100

    snippets everywhere

--- client/src/components/submission/comment/CommentThread.tsx
+++ client/src/components/submission/comment/CommentThread.tsx
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
 
-import {Snippet as ModelSnippet} from "../../../placeholdermodels";
 import {Comment as CommentComponent} from "./Comment";
-import {Snippet} from "./Snippet";
 import {WriteComment} from "./WriteComment";
 import {ButtonBar} from "../../general/ButtonBar";
 import { Button } from "react-bootstrap";
@@ -11,6 +9,7 @@ import { CommentThread } from "../../../../../models/api/CommentThread";
 import { JsonFetchError } from "../../../../helpers/FetchHelper";
 import { createComment } from "../../../../helpers/APIHelper";
 import {File} from "../../../../../models/api/File";
+import {Snippet} from "./Snippet";
 
 interface CommentThreadProperties {
 	/** The id for the CommentThread in the databaseRoutes */
@@ -18,63 +17,12 @@ interface CommentThreadProperties {
 	submissionID? : string,
 	file? : File,
 	body? : string
-	// Maybe also find a way to include the topic, so it can be shown immediately
 }
-
-/** Amount of lines to display when minimized*/
-const MINIMIZED_LINES = 3;
-/** Amount of lines inclusive that count as a small snippet */
-const SMALL_SNIPPET_LINES = 3;
-/** Amount of context lines to show above for a small snippet */
-const SMALL_SNIPPET_LINES_ABOVE = 2;
-/** Amount of context lines to show below for a small snippet */
-const SMALL_SNIPPET_LINES_BELOW = 2;
-/** Amount of context lines to show above for a small snippet */
-const LARGE_SNIPPET_LINES_ABOVE = 0;
-/** Amount of context lines to show below for a small snippet */
-const LARGE_SNIPPET_LINES_BELOW = 0;
 
 export function CommentThread({submissionID, thread, body, file}: CommentThreadProperties) {
 	const [opened, setOpened] = useState(false);
     const [comments, updateComments] = useState(thread.comments);
     const [newCommentText, updateNewCommentText] = useState("");
-
-	let snippet : ModelSnippet | undefined = undefined;
-	if (thread.snippet != undefined && body != undefined && file != undefined) {
-		// Parse body into lines
-		const fileContent : string[] = body.replace('\r', '').split('\n');
-		const totalLines : number = fileContent.length;
-
-		// Get snippet data
-		const threadSnippet = thread.snippet;
-		const lineStart : number = threadSnippet.start.line;
-		const lineEnd : number = threadSnippet.end.line;
-
-		// Get margins for the snippet
-		const snippetLength : number = (lineEnd - lineStart + 1);
-		const smallSnippet : boolean = snippetLength <= SMALL_SNIPPET_LINES;
-		let topMargin : number = smallSnippet ? SMALL_SNIPPET_LINES_ABOVE : LARGE_SNIPPET_LINES_ABOVE;
-		let bottomMargin : number = smallSnippet ? SMALL_SNIPPET_LINES_BELOW : LARGE_SNIPPET_LINES_BELOW;
-		topMargin = Math.min(topMargin, lineStart);
-		bottomMargin = Math.min(bottomMargin, totalLines - 1 - lineEnd);
-
-		// Corresponding lines in the file
-		const fileLineStart = lineStart - topMargin;
-		const fileLineEnd = lineEnd + bottomMargin + 1;
-
-		// Main lines to highlight first
-		const mainLineStart = topMargin;
-		const mainLineEnd = topMargin + Math.min(MINIMIZED_LINES, snippetLength);
-
-		snippet = {
-			submissionId : submissionID,
-			fullText : fileContent.slice(fileLineStart, fileLineEnd),
-			mainLines : [mainLineStart, mainLineEnd],
-			fileId : file.ID,
-			fileLines : [fileLineStart, fileLineEnd]
-		};
-	}
-
 
 	const handleNewComment = async () => {
         if (newCommentText !== null && newCommentText.trim() !== "") {
@@ -82,7 +30,6 @@ export function CommentThread({submissionID, thread, body, file}: CommentThreadP
                 const comment = await createComment(thread.ID, {
                     commentBody : newCommentText
                 });
-                console.log('added comment response' + comment);
                 updateComments(comments => [
                     ...comments,
                     comment
@@ -99,11 +46,11 @@ export function CommentThread({submissionID, thread, body, file}: CommentThreadP
         }
 	};
 
-	//console.log(thread, snippet);
-	return (
+    return (
 		<div id={thread.ID} className="commentThread">
-			<div> {/* Assuming loading is always successful, obviously */}
-                {snippet && <Snippet snippet={snippet}/>}
+			<div>
+				{/* Assuming loading is always successful, obviously */}
+				{thread.snippet && <Snippet snippet={thread.snippet}/>}
                 {opened ? <div>
                         {comments.map(comment => <CommentComponent comment={comment}/>)}
                         <WriteComment placeholder="Reply..." text={newCommentText} updateText={updateNewCommentText} />
--- client/src/components/submission/comment/Snippet.tsx
+++ client/src/components/submission/comment/Snippet.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 
-import {Snippet} from "../../../placeholdermodels";
+import {Snippet} from "../../../../../models/api/Snippet";
 import {Link} from "react-router-dom";
 import {ButtonBar} from "../../general/ButtonBar";
 import {Button} from "react-bootstrap";
 import {FiChevronDown, FiChevronUp, FiCode} from "react-icons/all";
+import {MINIMIZED_LINES} from "../../../helpers/CommentHelper";
 
 interface SnippetProperties {
 	snippet: Snippet
@@ -12,22 +13,26 @@ interface SnippetProperties {
 
 export function Snippet({snippet}: SnippetProperties) {
 	const [expanded, updateExpanded] = useState(false);
+	const completeSnippet : string[] = snippet.body.split('\r').join("").split('\n');
 
-	const [begin, end] = snippet.mainLines;
-	const preLines = snippet.fullText.slice(0, begin);
-	const mainLines = snippet.fullText.slice(begin, end);
-	const postLines = snippet.fullText.slice(end);
+	// TODO top context required from database
+	// TODO bottom context required from database
+	const preLines : string[] = [];
+	const mainLines : string[] = completeSnippet.slice(0, Math.min(completeSnippet.length, MINIMIZED_LINES));
+	// Append spaces to the first line to match indentation (in case user starts selection at character 4 for example)
+	if (mainLines.length > 0) mainLines[0] = " ".repeat(snippet.start.character) + mainLines[0];
+	const postLines : string[] = completeSnippet.slice(MINIMIZED_LINES);
 
 	return (
 		<div className="snippet">
             <pre className="m-0 px-2 py-1">
-                {expanded && preLines.join("\n") + "\n"}
+                {(expanded && preLines.length > 0) && preLines.join("\n") + "\n"}
 	            <span>{mainLines.join("\n")}</span>
-	            {expanded && "\n" + postLines.join("\n")}
+	            {(expanded && postLines.length > 0) && "\n" + postLines.join("\n")}
             </pre>
 			<ButtonBar align="right">
 				<Button>
-					<Link to={`/submission/${snippet.submissionId}/${snippet.fileId}/code#${snippet.fileLines[0]}`}>
+					<Link to={`/submission/${snippet.references.submissionID}/${snippet.file.ID}/code#${snippet.start.line}`}>
 						<FiCode size={14} color="#FFFFFF"/>
 					</Link>
 				</Button>
