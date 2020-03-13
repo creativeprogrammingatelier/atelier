a84b2b209d5634ae3214fead8ea6ae6eddadbe54
Cas <c.t.sievers@student.utwente.nl>
2020-03-13 09:22:43 +0100

    Enter is now also submit, unless you hold shift. In that case, you jump to a new line, unless you're in a regular input field, since these don't do multiple lines

--- client/src/components/comment/CommentCreator.tsx
+++ client/src/components/comment/CommentCreator.tsx
@@ -8,7 +8,31 @@ interface CommentCreatorProperties {
 export function CommentCreator({sendHandler}: CommentCreatorProperties) {
 	const [comment, setComment] = useState("");
 	const [sending, setSending] = useState(false);
+	const [shift, setShift] = useState(false);
 
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		console.log("Pressed a key: "+event.key);
+		if (event.key === "Shift") {
+			setShift(true);
+		}
+		if (event.key === "Enter") {
+			// Pressing enter by default does some weird redirect thing, cancel that
+			event.preventDefault();
+			if (shift) {
+				// Instead, we want it to add a new line to the input if shift is held
+				// TODO: However, only textarea's accept new lines, so fix that at some point
+				setComment(comment => comment + "\n");
+			} else if (event.key === "Enter") {
+				// Or submit the comment if not. There is an undefined promise resolve to make warnings go away
+				handleCommentSubmit().then(undefined);
+			}
+		}
+	};
+	const handleKeyUp = (event: React.KeyboardEvent) => {
+		if (event.key === "Shift") {
+			setShift(false);
+		}
+	};
 	const handleCommentChange = (event: React.FormEvent<HTMLInputElement>)  => setComment((event.target as HTMLInputElement).value);
 	const handleCommentSubmit = async() => {
 		if (!sending) {
@@ -25,7 +49,7 @@ export function CommentCreator({sendHandler}: CommentCreatorProperties) {
 	return <Form>
 		<Form.Group>
 			<InputGroup className="bg-transparent">
-				<Form.Control type="text" placeholder="Reply..." className="bg-transparent" value={comment} onChange={handleCommentChange}/>
+				<Form.Control type="text" placeholder="Reply..." className="bg-transparent" value={comment} onChange={handleCommentChange} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}/>
 				<InputGroup.Append>
 					<Button onClick={handleCommentSubmit} disabled={sending}><FiSend size={14} color="#FFFFFF"/></Button>
 				</InputGroup.Append>
