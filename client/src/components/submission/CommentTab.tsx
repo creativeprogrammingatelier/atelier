bfc91ba8109ae567522beea98c29678079246cec
Cas <c.t.sievers@student.utwente.nl>
2020-03-16 15:22:37 +0100

    Added functionality to scroll to a hash in a url again

--- client/src/components/submission/CommentTab.tsx
+++ client/src/components/submission/CommentTab.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import {CommentThread} from "../comment/CommentThread";
 import {File} from "../../../../models/api/File";
 import {Loading} from "../general/loading/Loading";
 import {CommentThread as APICommentThread} from "../../../../models/api/CommentThread";
 import {getFileComments} from "../../../helpers/APIHelper";
+import {ScrollHelper} from "../../helpers/ScrollHelper";
 
 interface CommentTabProperties {
     body: string,
@@ -14,6 +15,8 @@ interface CommentTabProperties {
 
 // TODO sort threads once new interface is implemented. Useful for switch between code and comment threads.
 export function CommentTab({file}: CommentTabProperties) {
+	useEffect(() => ScrollHelper.scrollToHash(), []);
+
 	return <div className="contentTab">
 		<div className="m-3">
 			<Loading<APICommentThread[]>
---
+++ client/src/helpers/ScrollHelper.ts
@@ -0,0 +1,20 @@
+export class ScrollHelper {
+	static headerHeight = 70;
+	static scrollOffset = 15;
+
+	static scrollToID(id: string) {
+		const element = document.getElementById(id);
+		if (element) {
+			window.scrollTo({
+				top: element.getBoundingClientRect().top - this.headerHeight - this.scrollOffset,
+				behavior: "smooth"
+			});
+		}
+	}
+	static scrollToHash() {
+		const hash = window.location.hash;
+		if (hash) {
+			this.scrollToID(hash.substr(1));
+		}
+	}
+}
