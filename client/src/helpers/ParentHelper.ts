6351221f902eb12263304b0baa8950f30fc09946
Cas <c.t.sievers@student.utwente.nl>
2020-03-16 16:33:21 +0100

    Visuals for more comment thread functionality

--- client/src/helpers/ParentHelper.ts
+++ client/src/helpers/ParentHelper.ts
@@ -18,4 +18,13 @@ export interface ParentalProperties {
 		| Array<boolean | JSX.Element>
 		| Array<number | JSX.Element>
 		| Array<string | JSX.Element>
+		| Array<boolean | undefined | JSX.Element>
+		| Array<boolean | null | JSX.Element>
+		| Array<boolean | number | JSX.Element>
+		| Array<boolean | string | JSX.Element>
+		| Array<number | undefined | JSX.Element>
+		| Array<number | null | JSX.Element>
+		| Array<number | string | JSX.Element>
+		| Array<string | undefined | JSX.Element>
+		| Array<string | null | JSX.Element>
 }
