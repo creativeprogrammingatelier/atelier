f116cc52669b29e0653861e88c32c69c19256bce
Jarik <jarikgerbertkarsten@gmail.com>
2020-03-18 20:25:05 +0100

    comment thread visible if author of any comment in the thread

--- api/src/helpers/APIFilterHelper.ts
+++ api/src/helpers/APIFilterHelper.ts
@@ -6,6 +6,13 @@ import {Comment} from "../../../models/api/Comment";
 import {Submission} from "../../../models/api/Submission";
 import {threadState} from "../../../models/enums/threadStateEnum";
 
+/**
+ * Filters comment thread API response
+ * - Comment thread is returned if PermissionEnum.viewRestrictedComments is set in permissions
+ * - Comment thread is returned if user is the author of any comment in the comment thread
+ * @param commentThreads, comment threads sent back to user
+ * @param userID, ID of the requesting user
+ */
 export async function filterCommentThread(commentThreads : CommentThread[], userID : string) {
     if (commentThreads.length === 0) return commentThreads;
     const courseID : string = commentThreads[0].references.courseID;
@@ -13,7 +20,7 @@ export async function filterCommentThread(commentThreads : CommentThread[], user
     if (!containsPermission(PermissionEnum.viewRestrictedComments, permissions)) {
         return commentThreads.filter((commentThread : CommentThread) =>
             commentThread.visibility === threadState.public ||
-            userMatch(userID, commentThread)
+            userPartOfCommentThread(userID, commentThread)
         );
     }
     return commentThreads;
@@ -41,10 +48,14 @@ export async function filterComment(comments : Comment[], userID : string) {
 
 }
 
-function userMatch(userID : string, commentThread : CommentThread) {
+function userPartOfCommentThread(userID : string, commentThread : CommentThread) {
+    return commentThread.comments.some((comment : Comment) => comment.ID === userID);
+}
+
+function commentThreadOwner(commentThread : CommentThread) {
     const sorted : Comment[] = commentThread.comments.sort((a : Comment, b : Comment) => {
         return (new Date(a.date).getUTCMilliseconds()) - (new Date(b.date).getUTCMilliseconds());
     });
-    if (sorted.length === 0) return false;
-    return sorted[0].user.ID === userID;
+    if (sorted.length === 0) return undefined;
+    return sorted[0].user.ID;
 }
