62b03353739c073085b10bba29e88e508d12f806
Arthur Rump <hello@arthurrump.com>
2020-03-27 19:47:46 +0100

    Add option to show loading at the bottom of CachedList

--- client/src/components/general/loading/CachedList.tsx
+++ client/src/components/general/loading/CachedList.tsx
@@ -6,6 +6,7 @@ interface CachedListProperties<T> {
     collection: CacheCollection<T>, 
     refresh?: () => void,
     timeout?: number,
+    bottom?: boolean,
     children: (item: CacheItem<T>) => React.ReactNode
 }
 
@@ -13,7 +14,7 @@ function jitter(time: number) {
     return time + (Math.random() * time * 0.2) - (time * 0.1);
 }
 
-export function CachedList<T>({ collection, children, refresh, timeout = 120 }: CachedListProperties<T>) {
+export function CachedList<T>({ collection, children, refresh, timeout = 120, bottom = false }: CachedListProperties<T>) {
     useEffect(() => {
         if (refresh) {
             const expiration = Math.max(0, collection.lastRefresh + timeout * 1000 - Date.now());
@@ -24,8 +25,9 @@ export function CachedList<T>({ collection, children, refresh, timeout = 120 }: 
 
     return (
         <Fragment>
-            {collection.state !== CacheState.Loaded && <LoadingIcon />}
+            {collection.state !== CacheState.Loaded && !bottom && <LoadingIcon />}
             {collection.items.map(item => children(item))}
+            {collection.state !== CacheState.Loaded && bottom && <LoadingIcon />}
         </Fragment>
     );
 }
