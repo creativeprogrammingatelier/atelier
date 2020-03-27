6340db8b34976570262e6bd64579b677db79ebf0
Cas <c.t.sievers@student.utwente.nl>
2020-03-27 09:30:56 +0100

    Added link to settings for the course page

--- client/src/components/course/CourseOverview.tsx
+++ client/src/components/course/CourseOverview.tsx
@@ -3,13 +3,17 @@ import {Frame} from "../frame/Frame";
 import {DataBlockList} from "../data/DataBlockList";
 import {Loading} from "../general/loading/Loading";
 import {Submission} from "../../../../models/api/Submission";
-import {coursePermission, getCourse, getCourseMentions, getCourseSubmissions} from "../../../helpers/APIHelper";
+import {coursePermission, getCourse, getCourseMentions, getCourseSubmissions, permission} from "../../../helpers/APIHelper";
 import {Uploader} from "../uploader/Uploader";
-import {Jumbotron} from "react-bootstrap";
+import {Button, Jumbotron} from "react-bootstrap";
 import {Course, CoursePartial} from "../../../../models/api/Course";
 import {Mention} from "../../../../models/api/Mention";
-import {Permission} from "../../../../models/api/Permission";
+import {CoursePermission, Permission} from "../../../../models/api/Permission";
 import {FiPlus, FiX} from "react-icons/all";
+import {containsPermission, PermissionEnum} from "../../../../models/enums/permissionEnum";
+import {DataList} from "../data/DataList";
+import {PluginSettings} from "../settings/PluginSettings";
+import {Link} from "react-router-dom";
 
 interface CourseOverviewProps {
 	match: {
@@ -42,7 +46,31 @@ export function CourseOverview({match: {params: {courseId}}}: CourseOverviewProp
 					params={[courseId]}
 					component={course => <Fragment><h1>{course.name}</h1><p>Created by {course.creator.name}</p></Fragment>}
 				/>
+				<Loading<Permission>
+					loader={coursePermission}
+					params={[courseId]}
+					component={permission => containsPermission(PermissionEnum.manageCourses, permission.permissions) && <Link to={`/course/${courseId}/settings`}><Button>Settings</Button></Link>}
+				/>
 			</Jumbotron>
+			<Loading<Mention[]>
+				loader={courseID => getCourseMentions(courseID)}
+				params={[courseId]}
+				component={mentions =>
+					<DataBlockList
+						header="Mentions"
+						list={mentions.map(mention => ({
+							transport:
+								mention.comment.references.fileID !== undefined
+									? `/submission/${mention.references.submissionID}/${mention.comment.references.fileID}/comments#${mention.comment.references.commentThreadID}`
+									: `/submission/${mention.references.submissionID}#${mention.comment.references.commentThreadID}`,
+							title: `Mentioned by ${mention.comment.user.name} on ${mention.submissionTitle}`,
+							text: mention.comment.text,
+							time: new Date(mention.comment.created),
+							tags: []
+						}))}
+					/>
+				}
+			/>
 			<Loading<Submission[]>
 				loader={(courseId, reload) => getCourseSubmissions(courseId, false)}
 				params={[courseId, reload]}
@@ -67,25 +95,6 @@ export function CourseOverview({match: {params: {courseId}}}: CourseOverviewProp
 								time: new Date(submission.date)
 							};
 						})}
-					/>
-				}
-			/>
-			<Loading<Mention[]>
-				loader={courseID => getCourseMentions(courseID)}
-				params={[courseId]}
-				component={mentions =>
-					<DataBlockList
-						header="Mentions"
-						list={mentions.map(mention => ({
-							transport:
-								mention.comment.references.fileID !== undefined
-									? `/submission/${mention.references.submissionID}/${mention.comment.references.fileID}/comments#${mention.comment.references.commentThreadID}`
-									: `/submission/${mention.references.submissionID}#${mention.comment.references.commentThreadID}`,
-							title: `Mentioned by ${mention.comment.user.name} on ${mention.submissionTitle}`,
-							text: mention.comment.text,
-							time: new Date(mention.comment.created),
-							tags: []
-						}))}
 					/>
 				}
 			/>
