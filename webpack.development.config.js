12b76da4692e6e3607cb5359dda51956701be9da
Arthur Rump <hello@arthurrump.com>
2020-04-01 18:35:56 +0200

    Exclude node-modules from sourcemap-loader
    This fixes the observable-hooks warnings

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -43,6 +43,7 @@ module.exports = {
             {
                 enforce: "pre",
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: "source-map-loader"
             },
             {
