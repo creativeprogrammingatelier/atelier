4a0377e39d91296fabc444843f56a6a2daecec75
Jarik <jarikgerbertkarsten@gmail.com>
2020-04-08 16:40:15 +0200

    redirecting

--- client/src/components/settings/system/CourseCreator.tsx
+++ client/src/components/settings/system/CourseCreator.tsx
@@ -3,7 +3,7 @@ import {Button, Form} from "react-bootstrap";
 
 import {Course} from "../../../../../models/api/Course";
 import {CourseState} from "../../../../../models/enums/CourseStateEnum";
-
+import {useHistory} from "react-router-dom";
 import {useCourses} from "../../../helpers/api/APIHooks";
 
 import {FeedbackError} from "../../feedback/FeedbackError";
@@ -18,15 +18,17 @@ export function CourseCreator({handleResponse}: CourseCreatorProperties) {
     const [courseName, setCourseName] = useState("");
     const [error, setError] = useState(false as FeedbackContent);
     const courses = useCourses();
+    const history = useHistory();
 
     // Create course
     async function handleSubmission(courseName : string) {
         try {
-            await courses.create({
+            const course = await courses.create({
                 name : courseName,
                 state : CourseState.open
             });
             setCourseName("");
+            history.push(`/course/${course.ID}/settings`);
         } catch (error) {
             setError(`Failed to create new course: ${error}`);
         }
