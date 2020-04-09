d94009e923f55181e2df9b321b68f6b14090d02f
Arthur Rump <hello@arthurrump.com>
2020-04-09 11:32:55 +0200

    Fix UUIDs

--- api/src/helpers/UUIDHelper.ts
+++ api/src/helpers/UUIDHelper.ts
@@ -13,9 +13,9 @@ export class UUIDHelper {
         if (id.length !== 22) throw new UUIDError(id);
 		/*
 			Convert a 22-character base 64 ID to the database internal UUID.
-			We use _ instead of the base 64 /
+			We use _ instead of the base 64 / and - instead of +
 		 */
-		const buffer = Buffer.from(id.replace(/_/g, "/"), "base64");
+		const buffer = Buffer.from(id.replace(/_/g, "/").replace(/-/g, "+"), "base64");
 		const hex = buffer.toString("hex");
 
 		return hex.substr(0, 8) + "-" + hex.substr(8, 4) + "-" + hex.substr(12, 4) + "-" + hex.substr(16, 4) + "-" + hex.substr(20, 12);
@@ -27,12 +27,12 @@ export class UUIDHelper {
 		if (uuid === undefined || uuid === null) return undefined
 		/*
 			Convert the internal database UUID to a 22-character shorter base 64 id.
-			We use _ instead of the base 64 /
+			We use _ instead of the base 64 / and - instead of +
 		 */
 		const hex = uuid.replace(/-/g, "");
 		const buffer = Buffer.from(hex, "hex");
 
-		return buffer.toString("base64").replace(/\//g, "_").substr(0, 22);
+		return buffer.toString("base64").replace(/\//g, "_").replace(/+/g, "-").substr(0, 22);
 	}
 }
 
