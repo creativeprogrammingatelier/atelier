6097638e93076b3a34b439aaf4f97dc314e4e072
Arthur Rump <hello@arthurrump.com>
2020-04-10 15:41:36 +0200

    Simplify AuthenticatedRoute
    Completely eliminates the waiting message, does not solve the redirect bug
    with Samling that happens in Safari and Firefox.

--- client/src/components/AuthenticatedRoute.tsx
+++ client/src/components/AuthenticatedRoute.tsx
@@ -1,40 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Redirect, Route} from 'react-router-dom';
 import {RouteComponentProps} from 'react-router';
 
 import AuthHelper from '../../helpers/AuthHelper';
 
 interface LocationProperties {
-	pathname: string
+    pathname: string
 }
 interface AuthenticatedRouteProperties<T> {
-	path: string,
-	component: React.ComponentType<RouteComponentProps<T>> | React.ComponentType<T>,
-	roles?: string[],
-	location?: LocationProperties
+    path: string,
+    component: React.ComponentType<RouteComponentProps<T>> | React.ComponentType<T>,
+    location?: LocationProperties
 }
-export function AuthenticatedRoute<T>({path, component, roles, location}: AuthenticatedRouteProperties<T>) {
-	const [authenticated, setAuthenticated] = useState(undefined as boolean | undefined);
-
-	useEffect(() => {
-		if (roles !== undefined) {
-			AuthHelper.checkRoles(roles).then((response: {status: number}) => {
-				setAuthenticated(response.status === 204);
-			});
-		} else {
-			setAuthenticated(true);
-		}
-	}, []);
-
-	if (authenticated !== undefined) {
-		if ((authenticated || roles === undefined) && AuthHelper.loggedIn()) {
-			return <Route path={path} component={component}/>
-		}
-		if (AuthHelper.loggedIn()) {
-			return <Redirect to="/"/>
-		}
-		return <Redirect to={{pathname: '/login', state: {from: location ? location.pathname : path}}}/>
-	}
-	return <p>Waiting</p> // TODO: Make more user friendly
+export function AuthenticatedRoute<T>({path, component, location}: AuthenticatedRouteProperties<T>) {
+    if (AuthHelper.loggedIn()) {
+        return <Route path={path} component={component}/>;
+    } else {
+        return <Redirect to={{pathname: '/login', state: {from: location ? location.pathname : path}}}/>
+    }
 }
-
