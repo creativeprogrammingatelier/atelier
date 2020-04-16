bfcd98c5992c5647eeba4f238742d2abc81a4fa6
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 14:11:39 +0200

    Formatting client/src/components/share

--- client/src/components/share/Sharing.tsx
+++ client/src/components/share/Sharing.tsx
@@ -5,21 +5,20 @@ import {CopyHelper} from "../../helpers/CopyHelper";
 import {QRCode} from "./QrCode";
 
 interface SharingProperties {
-    url: string
+	url: string
 }
-
 export function Sharing({url}: SharingProperties) {
-    function selectURL(event: React.FocusEvent<HTMLInputElement>) {
-        event.target.select();
-    }
-
-    return <div>
-        <InputGroup className="mb-3">
-            <FormControl plaintext readOnly defaultValue={url} onFocus={selectURL}/>
-            <InputGroup.Append>
-                <Button onClick={() => CopyHelper.copy(url)}>Copy <FiClipboard/></Button>
-            </InputGroup.Append>
-        </InputGroup>
-        <QRCode url={url}/>
-    </div>
+	const selectURL = (event: React.FocusEvent<HTMLInputElement>) => {
+		event.target.select();
+	};
+	
+	return <div>
+		<InputGroup className="mb-3">
+			<FormControl plaintext readOnly defaultValue={url} onFocus={selectURL}/>
+			<InputGroup.Append>
+				<Button onClick={() => CopyHelper.copy(url)}>Copy <FiClipboard/></Button>
+			</InputGroup.Append>
+		</InputGroup>
+		<QRCode url={url}/>
+	</div>;
 }
