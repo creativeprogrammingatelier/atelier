6a472bfd3304faeef58ac630b300803f6b995120
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 13:47:42 +0200

    Formatting client/src/components/search

--- client/src/components/search/SearchOverview.tsx
+++ client/src/components/search/SearchOverview.tsx
@@ -6,32 +6,25 @@ import {SearchResult} from "../../../../models/api/SearchResult";
 import {SearchResults} from "./SearchResults";
 
 export interface SearchProperties {
-    course?: string,
-    user?: string,
-    submission?: string
+	course?: string,
+	user?: string,
+	submission?: string
 }
-
 interface SearchOverviewProperties {
-    location: {
-        state: SearchProperties
-    }
+	location: {
+		state: SearchProperties
+	}
 }
-
 export function SearchOverview({location: {state}}: SearchOverviewProperties) {
-    const [searchResults, setSearchResults] = useState(undefined as unknown as SearchResult);
-
-    console.log("Loading search");
-    console.log(state);
-
-    return (
-        <Frame title="Search" sidebar>
-            <Jumbotron>
-                <h1>Search</h1>
-            </Jumbotron>
-            <div className="m-3">
-                <SearchQuery state={state} handleResponse={results => setSearchResults(results)}/>
-            </div>
-            {searchResults && <SearchResults results={searchResults}/>}
-        </Frame>
-    );
+	const [searchResults, setSearchResults] = useState(undefined as unknown as SearchResult);
+	
+	return <Frame title="Search" sidebar>
+		<Jumbotron>
+			<h1>Search</h1>
+		</Jumbotron>
+		<div className="m-3">
+			<SearchQuery state={state} handleResponse={results => setSearchResults(results)}/>
+		</div>
+		{searchResults && <SearchResults results={searchResults}/>}
+	</Frame>;
 }
