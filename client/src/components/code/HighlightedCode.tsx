c04ab7983857653bd5240fba9158018641c84f7b
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 12:53:48 +0200

    Formatting client/src/components/code

--- client/src/components/code/HighlightedCode.tsx
+++ client/src/components/code/HighlightedCode.tsx
@@ -1,107 +1,107 @@
 import React, {useEffect, useState} from "react";
-import {Code, CodeProperties, defaultHandler} from "./Code";
-import {getRanges, Range} from "../../helpers/HighlightingHelper";
 import {Controlled as CodeMirror} from "react-codemirror2";
+
 import {noPosition, Snippet} from "../../../../models/api/Snippet";
-import {SelectionHelper} from "../../helpers/SelectionHelper";
+
 import {ClickHelper} from "../../helpers/ClickHelper";
+import {getRanges, Range} from "../../helpers/HighlightingHelper";
+import {SelectionHelper} from "../../helpers/SelectionHelper";
+
+import {Code, CodeProperties, defaultHandler} from "./Code";
 
 export interface SnippetHighlight extends Snippet {
-    onClick: Function,
+	onClick: Function,
 }
-
 export interface HighlightedCodeProperties extends CodeProperties {
-    snippets: SnippetHighlight[]
+	snippets: SnippetHighlight[]
 }
-
 export function HighlightedCode({code, options, snippets, handleInitialize = defaultHandler, handleSelect = defaultHandler, handleClick = defaultHandler, handleChange = defaultHandler}: HighlightedCodeProperties) {
-    const [codeMirror, setCodeMirror] = useState(undefined as unknown as CodeMirror.Editor);
-    const [click, setClick] = useState(noPosition);
-
-    /**
-     * Highlights comments passed to the code viewer.
-     */
-    const setCommentHighlights = () => {
-        const color = "#3fdac1";
-        const opacityRange = ["00", "6F", "BF", "FF"];
-
-        // Highlight based on ranges
-        if (codeMirror && snippets) {
-            // Transform each snippet into a range of characters
-            const ranges: Range[] = snippets.map(snippet => {
-                return {
-                    startLine: snippet.start.line,
-                    startChar: snippet.start.character,
-                    endLine: snippet.end.line,
-                    endChar: snippet.end.character,
-                    overlap: 1
-                };
-            });
-
-            // Take care of overlap and things between the different ranges
-            const highlightRanges: Range[] = getRanges(ranges);
-
-            for (const {startLine, startChar, endLine, endChar, overlap} of highlightRanges) {
-                const opacity = opacityRange[Math.min(overlap, opacityRange.length - 1)];
-
-                codeMirror.markText({
-                    line: startLine, ch: startChar
-                }, {
-                    line: endLine, ch: endChar
-                }, {
-                    css: `background-color: ${color}${opacity};`
-                });
-            }
-        }
-    };
-
-    /**
-     * Handle click in the code canvas.
-     * Loops through the comments to check whether a comment was clicked. If this is
-     * the case the first comment will have its onClick method called.
-     */
-    const clickComment = () => {
-        if (snippets && click !== noPosition) {
-            let topPriority: SnippetHighlight | undefined = undefined;
-
-            // Find the topPriority snippet matching the click location
-            for (const snippet of snippets) {
-                console.log("Checking snippet");
-                console.log(snippet);
-                if (SelectionHelper.in(snippet, click)) {
-                    if (SelectionHelper.priority(snippet, topPriority)) {
-                        topPriority = snippet;
-                    }
-                }
-            }
-
-            // Call on click for comment
-            if (topPriority) {
-                topPriority.onClick();
-            }
-        }
-    };
-
-
-    useEffect(setCommentHighlights, [codeMirror, snippets]);
-    useEffect(clickComment, [click]);
-
-    return <Code
-        code={code}
-        options={options}
-        handleInitialize={
-            editor => {
-                handleInitialize(editor);
-                setCodeMirror(editor);
-            }}
-        handleSelect={handleSelect}
-        handleClick={(editor, event) => {
-            if (handleClick(editor, event) !== false) {
-                const pagePosition = ClickHelper.pagePosition(event);
-                const cursor = editor.coordsChar({left: pagePosition.x, top: pagePosition.y});
-                setClick({line: cursor.line, character: cursor.ch});
-            }
-        }}
-        handleChange={handleChange}
-    />;
+	const [codeMirror, setCodeMirror] = useState(undefined as unknown as CodeMirror.Editor);
+	const [click, setClick] = useState(noPosition);
+	
+	/**
+	 * Highlights comments passed to the code viewer.
+	 */
+	const setCommentHighlights = () => {
+		const color = "#3FDAC1";
+		const opacityRange = ["00", "6F", "BF", "FF"];
+		
+		// Highlight based on ranges
+		if (codeMirror && snippets) {
+			// Transform each snippet into a range of characters
+			const ranges: Range[] = snippets.map(snippet => {
+				return {
+					startLine: snippet.start.line,
+					startChar: snippet.start.character,
+					endLine: snippet.end.line,
+					endChar: snippet.end.character,
+					overlap: 1
+				};
+			});
+			
+			// Take care of overlap and things between the different ranges
+			const highlightRanges: Range[] = getRanges(ranges);
+			
+			for (const {startLine, startChar, endLine, endChar, overlap} of highlightRanges) {
+				const opacity = opacityRange[Math.min(overlap, opacityRange.length - 1)];
+				
+				codeMirror.markText({
+					line: startLine, ch: startChar
+				}, {
+					line: endLine, ch: endChar
+				}, {
+					css: `background-color: ${color}${opacity};`
+				});
+			}
+		}
+	};
+	
+	/**
+	 * Handle click in the code canvas.
+	 * Loops through the comments to check whether a comment was clicked. If this is
+	 * the case the first comment will have its onClick method called.
+	 */
+	const clickComment = () => {
+		if (snippets && click !== noPosition) {
+			let topPriority: SnippetHighlight | undefined = undefined;
+			
+			// Find the topPriority snippet matching the click location
+			for (const snippet of snippets) {
+				console.log("Checking snippet");
+				console.log(snippet);
+				if (SelectionHelper.in(snippet, click)) {
+					if (SelectionHelper.priority(snippet, topPriority)) {
+						topPriority = snippet;
+					}
+				}
+			}
+			
+			// Call on click for comment
+			if (topPriority) {
+				topPriority.onClick();
+			}
+		}
+	};
+	
+	useEffect(setCommentHighlights, [codeMirror, snippets]);
+	useEffect(clickComment, [click]);
+	
+	return <Code
+		code={code}
+		options={options}
+		handleInitialize={
+			editor => {
+				handleInitialize(editor);
+				setCodeMirror(editor);
+			}}
+		handleSelect={handleSelect}
+		handleClick={(editor, event) => {
+			if (handleClick(editor, event) !== false) {
+				const pagePosition = ClickHelper.pagePosition(event);
+				const cursor = editor.coordsChar({left: pagePosition.x, top: pagePosition.y});
+				setClick({line: cursor.line, character: cursor.ch});
+			}
+		}}
+		handleChange={handleChange}
+	/>;
 }
--- client/src/components/code/Snippet.tsx
+++ client/src/components/code/Snippet.tsx
@@ -1,31 +1,29 @@
 import React from "react";
-
 import {Snippet} from "../../../../models/api/Snippet";
 import {MINIMIZED_LINES} from "../../../../helpers/SnippetHelper";
 import {Code} from "./Code";
 
 interface SnippetProperties {
-    snippet: Snippet,
-    expanded?: boolean
+	snippet: Snippet,
+	expanded?: boolean
 }
-
 export function Snippet({snippet, expanded}: SnippetProperties) {
-    const completeSnippet: string[] = snippet.body.split("\r").join("").split("\n");
-
-    const databaseTop: string = snippet.contextBefore;
-    const linesTop: number = (databaseTop.split("\r").join("").split("\n").length);
-    const databaseBottom: string = snippet.contextAfter;
-
-    const preLines: string = databaseTop;
-    const mainLines: string = completeSnippet.slice(0, Math.min(completeSnippet.length, MINIMIZED_LINES)).join("\n");
-    const postLines: string = completeSnippet.slice(MINIMIZED_LINES).join("\n") + databaseBottom;
-
-    return <Code
-        code={expanded ? preLines + mainLines + postLines : mainLines}
-        options={{
-            mode: snippet.file.type,
-            firstLineNumber: expanded ? snippet.start.line + 2 - linesTop : snippet.start.line + 1,
-            readOnly: "nocursor"
-        }}
-    />;
+	const completeSnippet: string[] = snippet.body.split("\r").join("").split("\n");
+	
+	const databaseTop: string = snippet.contextBefore;
+	const linesTop: number = (databaseTop.split("\r").join("").split("\n").length);
+	const databaseBottom: string = snippet.contextAfter;
+	
+	const preLines: string = databaseTop;
+	const mainLines: string = completeSnippet.slice(0, Math.min(completeSnippet.length, MINIMIZED_LINES)).join("\n");
+	const postLines: string = completeSnippet.slice(MINIMIZED_LINES).join("\n") + databaseBottom;
+	
+	return <Code
+		code={expanded ? preLines + mainLines + postLines : mainLines}
+		options={{
+			mode: snippet.file.type,
+			firstLineNumber: expanded ? snippet.start.line + 2 - linesTop : snippet.start.line + 1,
+			readOnly: "nocursor"
+		}}
+	/>;
 }
