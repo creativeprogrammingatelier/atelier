c1c9b2471176fcaeb429e840010c8cf3a8c054b9
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 11:06:18 +0200

    Formatting test/

--- test/QuickCheck.ts
+++ test/QuickCheck.ts
@@ -5,24 +5,24 @@ export const TEST_COUNT = 200;
 
 /** Generate a random array of maximum length */
 export function randomNumberArray(max: number) {
-    const count = Math.floor(Math.random() * max);
-    const array = [];
-    for (let i = 0; i < count; i++) {
-        array[i] = Math.floor(Math.random() * max);
-    }
-    return array;
+	const count = Math.floor(Math.random() * max);
+	const array = [];
+	for (let i = 0; i < count; i++) {
+		array[i] = Math.floor(Math.random() * max);
+	}
+	return array;
 }
 
 /** Generate a random boolean */
 export function randomBool() {
-    return Math.random() > 0.5;
+	return Math.random() > 0.5;
 }
 
 /** Mocha's it, but repeated TEST_COUNT times */
 export function repeatIt(title: string, fn: () => void, count = TEST_COUNT) {
-    return it(title, () => {
-        for (let i = 0; i < count; i++) {
-            fn();
-        }
-    });
+	return it(title, () => {
+		for (let i = 0; i < count; i++) {
+			fn();
+		}
+	});
 }
--- test/plugin/WebhookHelper.spec.ts
+++ test/plugin/WebhookHelper.spec.ts
@@ -1,46 +1,43 @@
-import 'mocha';
-import {expect} from 'chai';
-import crypto from 'crypto';
+import {expect} from "chai";
+import crypto from "crypto";
+import "mocha";
 
-import {plugin} from './Plugin';
-import {createWebhookRequest} from '../../api/src/helpers/WebhookHelper';
-import {WebhookEvent} from '../../models/enums/WebhookEventEnum';
+import {WebhookEvent} from "../../models/enums/WebhookEventEnum";
+import {createWebhookRequest} from "../../api/src/helpers/WebhookHelper";
+import {plugin} from "./Plugin";
 
 describe("WebhookHelper.createWebhookRequest", () => {
-    const event = WebhookEvent.SubmissionFile;
-    const body = {
-        test: "value",
-        other: {
-            thing: 4
-        }
-    };
-
-    it("should set the user agent", () => {
-        const req = createWebhookRequest(plugin, event, body);
-        expect(req.headers.get("User-Agent"), "User-Agent").to.equal("Atelier");
-    });
-
-    it("should create a signed request", async () => {
-        const req = createWebhookRequest(plugin, event, body);
-        const reqBody = await req.text();
-        const signature = crypto.createHmac("sha1", plugin.webhookSecret).update(reqBody).digest("base64");
-
-        expect(req.headers.get("X-Atelier-Signature"), "X-Atelier-Signature").to.equal(signature);
-    });
-
-    it("should set the correct event and payload", async () => {
-        const req = createWebhookRequest(plugin, event, body);
-        const reqBody = await req.json();
-
-        expect(reqBody.event).to.equal(event);
-        expect(reqBody.payload).to.deep.equal(body);
-    });
-
-    it("should post to the defined url", () => {
-        const req = createWebhookRequest(plugin, event, body);
-        expect(req.method).to.equal("POST");
-        expect(req.url).to.equal(plugin.webhookUrl);
-    });
+	const event = WebhookEvent.SubmissionFile;
+	const body = {
+		test: "value",
+		other: {
+			thing: 4
+		}
+	};
+	
+	it("should set the user agent", () => {
+		const req = createWebhookRequest(plugin, event, body);
+		expect(req.headers.get("User-Agent"), "User-Agent").to.equal("Atelier");
+	});
+	it("should create a signed request", async() => {
+		const req = createWebhookRequest(plugin, event, body);
+		const reqBody = await req.text();
+		const signature = crypto.createHmac("sha1", plugin.webhookSecret).update(reqBody).digest("base64");
+		
+		expect(req.headers.get("X-Atelier-Signature"), "X-Atelier-Signature").to.equal(signature);
+	});
+	it("should set the correct event and payload", async() => {
+		const req = createWebhookRequest(plugin, event, body);
+		const reqBody = await req.json();
+		
+		expect(reqBody.event).to.equal(event);
+		expect(reqBody.payload).to.deep.equal(body);
+	});
+	it("should post to the defined url", () => {
+		const req = createWebhookRequest(plugin, event, body);
+		expect(req.method).to.equal("POST");
+		expect(req.url).to.equal(plugin.webhookUrl);
+	});
 });
 
 // describe("WebhookHelper.getSubscribedPlugins", () => {
