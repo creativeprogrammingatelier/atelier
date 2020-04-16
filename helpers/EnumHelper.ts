ead7b28c92e57c5dbc24ed2e9fe92ee387e30ac3
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 14:50:26 +0200

    Formatting helpers/

--- helpers/EnumHelper.ts
+++ helpers/EnumHelper.ts
@@ -1,46 +1,46 @@
 /**
  * A function to make sure a value is contained in an enum, this returns the correct enum value
  * If the given enum is of type <string>=<sameString>, this function can be used multiple times without issue.
- * 
+ *
  * @param enumer the enum to select an item from
  * @param item the (maybe) item to be selected
  * @throws if the given item is not part of the given enumeration.
  */
-export function getEnum<T>(enumer : T, item : string) {
-	if (checkEnum(enumer, item)){
+export function getEnum<T>(enumer: T, item: string) {
+	if (checkEnum(enumer, item)) {
 		return enumer[item];
 	} else {
-		const items = '{'+Object.keys(enumer).join(', ')+'}';
-		throw new EnumError("item `"+item+"` was expected to be part of the enum "+items+", but wasn't")
+		const items = "{" + Object.keys(enumer).join(", ") + "}";
+		throw new EnumError("item `" + item + "` was expected to be part of the enum " + items + ", but wasn't");
 	}
 }
 
-export function checkEnum<T>(enumer : T, item : string | number | symbol) : item is keyof typeof enumer{
+export function checkEnum<T>(enumer: T, item: string | number | symbol): item is keyof typeof enumer {
 	return item in enumer;
 }
 
 //this function adds `item` to enum( : any) 
 // tslint:disable-next-line: no-any
-export function addItem(enumer : any, item : string) : void {
-	if (item in enumer){
-		throw new EnumError("tried to add an item to an enum that was already there: "+item+" , enum entries: "+enumer)
+export function addItem(enumer: any, item: string): void {
+	if (item in enumer) {
+		throw new EnumError("tried to add an item to an enum that was already there: " + item + " , enum entries: " + enumer);
 	}
 	enumer[item] = item;
 }
 
 //this function removes `item` from enum( : any) 
 // tslint:disable-next-line: no-any
-export function removeItem(enumer : any, item : string) : void {
-	if (!(item in enumer)){
-		throw new EnumError("tried to remove an item from an enum that was not there: "+item+" , enum entries: "+enumer)
+export function removeItem(enumer: any, item: string): void {
+	if (!(item in enumer)) {
+		throw new EnumError("tried to remove an item from an enum that was not there: " + item + " , enum entries: " + enumer);
 	}
-	delete enumer[item]
+	delete enumer[item];
 }
 /**
  * error class to differentiate errors thrown by this module
  */
 export class EnumError extends Error {
-	constructor(message = "expected a value to be a member of an enum, but this was not the case"){
-		super(message)
+	constructor(message = "expected a value to be a member of an enum, but this was not the case") {
+		super(message);
 	}
 }
--- helpers/ProjectValidationHelper.ts
+++ helpers/ProjectValidationHelper.ts
@@ -1,39 +1,37 @@
-import path from 'path';
+import path from "path";
 
-import './Extensions';
-import {CODEFILE_EXTENSIONS, MAX_FILE_SIZE, MAX_PROJECT_SIZE} from './Constants';
-
-import './Extensions'
+import {CODEFILE_EXTENSIONS, MAX_FILE_SIZE, MAX_PROJECT_SIZE} from "./Constants";
+import "./Extensions";
 
 // Interfaces
 /////////////
 /** Possible errors for invalid projects */
 export interface ProjectValidation<T extends Fileish<T>> {
-    /** The project doesn't contain any files with a code file extension */
-    containsNoCodeFiles: boolean,
-    /** The project has no file with the name of the project */
-    invalidProjectName: boolean,
-    /** The project as a whole (all files combined) is too large */
-    projectTooLarge: boolean,
-    /** List of filenames that are too large, or false if there are none */
-    acceptableFiles: T[]
+	/** The project doesn't contain any files with a code file extension */
+	containsNoCodeFiles: boolean,
+	/** The project has no file with the name of the project */
+	invalidProjectName: boolean,
+	/** The project as a whole (all files combined) is too large */
+	projectTooLarge: boolean,
+	/** List of filenames that are too large, or false if there are none */
+	acceptableFiles: T[]
 }
 
 /** Default values for projects with no errors */
 export function defaultValidation<T extends Fileish<T>>(files: T[]): ProjectValidation<T> {
-    return {
-        containsNoCodeFiles: false,
-        invalidProjectName: false,
-        projectTooLarge: false,
-        acceptableFiles: files
-    };
+	return {
+		containsNoCodeFiles: false,
+		invalidProjectName: false,
+		projectTooLarge: false,
+		acceptableFiles: files
+	};
 }
 
 /** Internal abstraction for files */
 interface ProjectFile<T> {
-    original: T,
-    pathInProject: string,
-    size: number
+	original: T,
+	pathInProject: string,
+	size: number
 }
 
 /** Is some sort of file */
@@ -43,73 +41,73 @@ type Fileish<T> = ProjectFile<T> | Express.Multer.File | File;
 //////////
 /** Error that can be thrown if the project has validation errors */
 export class ProjectValidationError<T extends Fileish<T>> extends Error {
-    validation: ProjectValidation<T>;
-
-    constructor(validation: ProjectValidation<T>, originalFiles: Array<ProjectFile<T>>) {
-        const message = "This project is not valid: " + [
-            validation.containsNoCodeFiles ? "it contains no code files" : undefined,
-            validation.invalidProjectName ? "it has no file with the name of the project" : undefined,
-            validation.projectTooLarge ? `it is larger than ${MAX_PROJECT_SIZE} bytes` : undefined,
-            validation.acceptableFiles.length !== originalFiles.length ? `it contains files that are larger than ${MAX_FILE_SIZE} bytes` : undefined
-        ].filter(x => x !== undefined).join(", ");
-        super(message);
-        this.validation = validation;
-    }
+	validation: ProjectValidation<T>;
+	
+	constructor(validation: ProjectValidation<T>, originalFiles: Array<ProjectFile<T>>) {
+		const message = "This project is not valid: " + [
+			validation.containsNoCodeFiles ? "it contains no code files" : undefined,
+			validation.invalidProjectName ? "it has no file with the name of the project" : undefined,
+			validation.projectTooLarge ? `it is larger than ${MAX_PROJECT_SIZE} bytes` : undefined,
+			validation.acceptableFiles.length !== originalFiles.length ? `it contains files that are larger than ${MAX_FILE_SIZE} bytes` : undefined
+		].filter(x => x !== undefined).join(", ");
+		super(message);
+		this.validation = validation;
+	}
 }
 
 // Validation checks
 ////////////////////
 function containsNoCodeFiles<T>(files: Array<ProjectFile<T>>) {
-    return !files.some(f => CODEFILE_EXTENSIONS.includes(path.extname(f.pathInProject)));
+	return !files.some(f => CODEFILE_EXTENSIONS.includes(path.extname(f.pathInProject)));
 }
 
 function invalidProjectName<T>(projectName: string, files: Array<ProjectFile<T>>) {
-    return !files.some(f => f.pathInProject === `${projectName}/${projectName}.pde` || f.pathInProject === `${projectName}.pde`);
+	return !files.some(f => f.pathInProject === `${projectName}/${projectName}.pde` || f.pathInProject === `${projectName}.pde`);
 }
 
 function projectTooLarge<T>(files: Array<ProjectFile<T>>) {
-    return files.reduce((sum, next) => sum + next.size, 0) >= MAX_PROJECT_SIZE;
+	return files.reduce((sum, next) => sum + next.size, 0) >= MAX_PROJECT_SIZE;
 }
 
 function acceptableFiles<T>(files: Array<ProjectFile<T>>) {
-    return files.filter(f => f.size < MAX_FILE_SIZE);
+	return files.filter(f => f.size < MAX_FILE_SIZE);
 }
 
 // Validation execution
 ///////////////////////
 /** Execute all checks and return the result */
 function validateProjectInternal<T extends Fileish<T>>(projectName: string, files: Array<ProjectFile<T>>): ProjectValidation<T> {
-    const acceptable = acceptableFiles(files);
-    return {
-        containsNoCodeFiles: containsNoCodeFiles(acceptable),
-        invalidProjectName: invalidProjectName(projectName, acceptable),
-        projectTooLarge: projectTooLarge(acceptable),
-        acceptableFiles: acceptable.map(f => f.original)
-    }
+	const acceptable = acceptableFiles(files);
+	return {
+		containsNoCodeFiles: containsNoCodeFiles(acceptable),
+		invalidProjectName: invalidProjectName(projectName, acceptable),
+		projectTooLarge: projectTooLarge(acceptable),
+		acceptableFiles: acceptable.map(f => f.original)
+	};
 }
 
 /** Execute all checks with files as they are modeled on the server, throwing an error if it is invalid */
 export function validateProjectServer(projectName: string, files: Express.Multer.File[]) {
-    const filesInternal = files.map(f => ({
-        original: f,
-        pathInProject: f.path.split(path.sep).skipWhile(folder => folder !== projectName).join("/"),
-        size: f.size
-    }));
-    const validation = validateProjectInternal(projectName, filesInternal);
-    if (validation.containsNoCodeFiles
-        || validation.invalidProjectName
-        || validation.projectTooLarge
-        || validation.acceptableFiles.length !== files.length) {
-        throw new ProjectValidationError(validation, filesInternal);
-    }
+	const filesInternal = files.map(f => ({
+		original: f,
+		pathInProject: f.path.split(path.sep).skipWhile(folder => folder !== projectName).join("/"),
+		size: f.size
+	}));
+	const validation = validateProjectInternal(projectName, filesInternal);
+	if (validation.containsNoCodeFiles
+		|| validation.invalidProjectName
+		|| validation.projectTooLarge
+		|| validation.acceptableFiles.length !== files.length) {
+		throw new ProjectValidationError(validation, filesInternal);
+	}
 }
 
 /** Execute all checks with files as they are modeled on the client, returning the validation result */
 export function validateProjectClient(projectName: string, files: File[]) {
-    const filesInternal = files.map(f => ({
-        original: f,
-        pathInProject: f.webkitRelativePath || `${projectName}/${f.name}`,
-        size: f.size
-    }));
-    return validateProjectInternal(projectName, filesInternal);
+	const filesInternal = files.map(f => ({
+		original: f,
+		pathInProject: f.webkitRelativePath || `${projectName}/${f.name}`,
+		size: f.size
+	}));
+	return validateProjectInternal(projectName, filesInternal);
 }
