6db02feb3537d08346cae52b0207f00734c18e5e
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 10:16:47 +0200

    Formatting api/src/lib and api/src/middleware

--- api/src/lib/constants.ts
+++ api/src/lib/constants.ts
@@ -1,21 +1,21 @@
-import path from 'path';
-import fs from 'fs';
-import crypto from 'crypto';
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
 
 // Private helpers
 const keyFilePath = path.join(__dirname, "../../keys/jwtRS256.key");
-let authKey = undefined;
+let authKey;
 if (!fs.existsSync(keyFilePath)) {
-    fs.mkdirSync(path.dirname(keyFilePath), {recursive: true});
-    console.log("Generating keys for signing JWT tokens.");
-    const {publicKey, privateKey} = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 4096
-    });
-    authKey = privateKey.export({format: "pem", type: "pkcs1"});
-    fs.writeFileSync(keyFilePath, authKey);
-    fs.writeFileSync(keyFilePath + ".pub", publicKey.export({format: "pem", type: "pkcs1"}));
+	fs.mkdirSync(path.dirname(keyFilePath), {recursive: true});
+	console.log("Generating keys for signing JWT tokens.");
+	const {publicKey, privateKey} = crypto.generateKeyPairSync("rsa", {
+		modulusLength: 4096
+	});
+	authKey = privateKey.export({format: "pem", type: "pkcs1"});
+	fs.writeFileSync(keyFilePath, authKey);
+	fs.writeFileSync(keyFilePath + ".pub", publicKey.export({format: "pem", type: "pkcs1"}));
 } else {
-    authKey = fs.readFileSync(keyFilePath, "utf8");
+	authKey = fs.readFileSync(keyFilePath, "utf8");
 }
 
 // Public constants
--- api/src/middleware/AuthMiddleware.ts
+++ api/src/middleware/AuthMiddleware.ts
@@ -1,41 +1,41 @@
+import {RequestHandler} from "express";
+import {verifyToken, getToken, AuthError, setTokenCookie} from "../helpers/AuthenticationHelper";
+import {captureNext} from "../helpers/ErrorHelper";
+
 /**
  * Middleware for checking user authentication and authorization
  * Author: Andrew Heath, Arthur Rump
  * Created: 13/08/19
  */
-import {verifyToken, getToken, AuthError, setTokenCookie} from '../helpers/AuthenticationHelper';
-import {RequestHandler} from 'express';
-import {captureNext} from '../helpers/ErrorHelper';
-
 export class AuthMiddleware {
-    /** Middleware function that will refresh tokens in cookies */
-    static refreshCookieToken: RequestHandler = captureNext(async (request, response, next) => {
-        if (request.cookies.atelierToken) {
-            try {
-                const token = await verifyToken<{ userID: string }>(request.cookies.atelierToken);
-                // The JWT expiration is stored in seconds, Date.now() in milliseconds
-                if (token.iat * 1000 + 600000 < Date.now()) {
-                    await setTokenCookie(response, token.userID);
-                }
-            } catch (err) {
-                // Ignore AuthErrors, if auth is required for the route, 
-                // it will be handled in requireAuth
-                if (!(err instanceof AuthError)) {
-                    throw err;
-                }
-            }
-        }
-        next();
-    });
-
-    /** Middleware function that will block all non-authenticated requests */
-    static requireAuth: RequestHandler = captureNext(async (request, _response, next) => {
-        const token = getToken(request);
-        if (!token) {
-            next(new AuthError("token.notProvided", "No token was provided with this request. You're probably not logged in."));
-        } else {
-            await verifyToken(token);
-            next();
-        }
-    });
+	/** Middleware function that will refresh tokens in cookies */
+	static refreshCookieToken: RequestHandler = captureNext(async(request, response, next) => {
+		if (request.cookies.atelierToken) {
+			try {
+				const token = await verifyToken<{userID: string}>(request.cookies.atelierToken);
+				// The JWT expiration is stored in seconds, Date.now() in milliseconds
+				if (token.iat * 1000 + 600000 < Date.now()) {
+					await setTokenCookie(response, token.userID);
+				}
+			} catch (err) {
+				// Ignore AuthErrors, if auth is required for the route,
+				// it will be handled in requireAuth
+				if (!(err instanceof AuthError)) {
+					throw err;
+				}
+			}
+		}
+		next();
+	});
+	
+	/** Middleware function that will block all non-authenticated requests */
+	static requireAuth: RequestHandler = captureNext(async(request, _response, next) => {
+		const token = getToken(request);
+		if (!token) {
+			next(new AuthError("token.notProvided", "No token was provided with this request. You're probably not logged in."));
+		} else {
+			await verifyToken(token);
+			next();
+		}
+	});
 }
