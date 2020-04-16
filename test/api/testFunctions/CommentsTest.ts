74d0815ae9072727f9aba9aebb9a41c53132f678
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 10:57:28 +0200

    Formatting test/api

--- test/api/testFunctions/CommentsTest.ts
+++ test/api/testFunctions/CommentsTest.ts
@@ -1,53 +1,55 @@
-import {
-    getCommentsUser,
-    USER_ID,
-    getCommentsByUserAndCourse,
-    COURSE_ID,
-    putComment,
-    adminRegisterCourse
-} from "../APIRequestHelper";
 import {expect} from "chai";
 import {assert} from "console";
-import {instanceOfComment} from "../../InstanceOf";
+
 import {Comment} from "../../../models/api/Comment";
 
+import {instanceOfComment} from "../../InstanceOf";
+import {
+	getCommentsUser,
+	USER_ID,
+	getCommentsByUserAndCourse,
+	COURSE_ID,
+	putComment,
+	adminRegisterCourse
+} from "../APIRequestHelper";
+
 export function commentTest() {
-    /**
-     * GET requests:
-     * /api/comment/user/:userID
-     * - response should be Comment[]
-     * - comments should belong to userID
-     * /api/comment/course/:courseID/user/:userID
-     * - response should be Comment[]
-     * - comments should belong to userID and courseID
-     */
-    describe("Comments", async () => {
-        it("Should be possible to get user comments", async () => {
-            const response = await getCommentsUser();
-            expect(response).to.have.status(200);
-            console.log("??????", response.body);
-            assert(response.body.every((comment: Comment) =>
-                instanceOfComment(comment) &&
-                comment.user.ID === USER_ID
-            ));
-        });
-
-        it("Should be possible to get user comments in a course", async () => {
-            const response = await getCommentsByUserAndCourse();
-            expect(response).to.have.status(200);
-            assert(response.body.every((comment: Comment) =>
-                instanceOfComment(comment) &&
-                comment.references.courseID === COURSE_ID &&
-                comment.user.ID === USER_ID
-            ));
-        });
-
-        it("Should be possible to post a comment", async () => {
-            await adminRegisterCourse();
-            const response = await putComment();
-            expect(response).to.have.status(200);
-            const comment = response.body;
-            assert(instanceOfComment(comment), "body should be comment, but was" + comment)
-        });
-    });
+	/**
+	 * GET requests:
+	 * /api/comment/user/:userID
+	 * - response should be Comment[]
+	 * - comments should belong to userID
+	 * /api/comment/course/:courseID/user/:userID
+	 * - response should be Comment[]
+	 * - comments should belong to userID and courseID
+	 */
+	describe("Comments", async() => {
+		it("Should be possible to get user comments", async() => {
+			const response = await getCommentsUser();
+			expect(response).to.have.status(200);
+			console.log("??????", response.body);
+			assert(response.body.every((comment: Comment) =>
+				instanceOfComment(comment) &&
+				comment.user.ID === USER_ID
+			));
+		});
+		
+		it("Should be possible to get user comments in a course", async() => {
+			const response = await getCommentsByUserAndCourse();
+			expect(response).to.have.status(200);
+			assert(response.body.every((comment: Comment) =>
+				instanceOfComment(comment) &&
+				comment.references.courseID === COURSE_ID &&
+				comment.user.ID === USER_ID
+			));
+		});
+		
+		it("Should be possible to post a comment", async() => {
+			await adminRegisterCourse();
+			const response = await putComment();
+			expect(response).to.have.status(200);
+			const comment = response.body;
+			assert(instanceOfComment(comment), "Body should be comment, but was" + comment);
+		});
+	});
 }
