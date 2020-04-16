0eca11665d563fddf538782625aadf24dd5b7b23
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 14:26:05 +0200

    Formatting client/src/components/tab

--- client/src/components/tab/TabBar.tsx
+++ client/src/components/tab/TabBar.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import {Nav} from 'react-bootstrap';
+import React from "react";
+import {Nav} from "react-bootstrap";
 import {IconType} from "react-icons";
 
-import {TabButton} from './TabButton';
+import {TabButton} from "./TabButton";
 
 interface TabProperties {
-    id?: string,
-    icon: IconType,
-    text: string,
-    location: string
+	id?: string,
+	icon: IconType,
+	text: string,
+	location: string
 }
-
 interface TabBarProperties {
-    tabs: TabProperties[],
-    active: string
+	tabs: TabProperties[],
+	active: string
 }
-
 export function TabBar({active, tabs}: TabBarProperties) {
-    return <Nav fill justify variant="pills" className="fixed-bottom position-sticky">
-        {tabs.map((tab) => <TabButton key={tab.id || tab.text} icon={tab.icon} text={tab.text} location={tab.location}
-                                      active={tab.id === active}/>)}
-    </Nav>
+	return <Nav fill justify variant="pills" className="fixed-bottom position-sticky">
+		{tabs.map((tab) =>
+			<TabButton
+				key={tab.id || tab.text}
+				icon={tab.icon}
+				text={tab.text}
+				location={tab.location}
+				active={tab.id === active}
+			/>
+		)}
+	</Nav>;
 }
