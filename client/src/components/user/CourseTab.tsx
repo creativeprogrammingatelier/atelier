cbd4941a74927f3a6f5df82100ea68c2727630b7
Cas <c.t.sievers@student.utwente.nl>
2020-04-16 14:35:45 +0200

    Formatting client/src/components/user

--- client/src/components/user/CourseTab.tsx
+++ client/src/components/user/CourseTab.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import {DataList} from "../data/DataList";
-import {Loading} from "../general/loading/Loading";
+
 import {Course} from "../../../../models/api/Course";
-import {getUserCourses} from "../../helpers/api/APIHelper";
-import {Panel} from "../general/Panel";
 import {User} from "../../../../models/api/User";
 
-interface CourseTabProperties {
-    user: User
-}
+import {getUserCourses} from "../../helpers/api/APIHelper";
 
+import {DataList} from "../data/DataList";
+import {Loading} from "../general/loading/Loading";
+import {Panel} from "../general/Panel";
+
+interface CourseTabProperties {
+	user: User
+}
 export function CourseTab({user}: CourseTabProperties) {
-    return <div className="contentTab">
-        <DataList header="Courses">
-            <Loading<Course[]>
-                loader={getUserCourses}
-                params={[user.ID]}
-                component={courses =>
-                    <div>
-                        {courses.map((course: Course) => <Panel
-                            key={course.ID}
-                            display={course.name}
-                            location={`/course/${course.ID}/user/${user.ID}`}
-                        />)}
-                    </div>
-                }
-            />
-        </DataList>
-    </div>
+	return <div className="contentTab">
+		<DataList header="Courses">
+			<Loading<Course[]>
+				loader={getUserCourses}
+				params={[user.ID]}
+				component={courses =>
+					<div>
+						{courses.map((course: Course) => <Panel
+							key={course.ID}
+							display={course.name}
+							location={`/course/${course.ID}/user/${user.ID}`}
+						/>)}
+					</div>
+				}
+			/>
+		</DataList>
+	</div>;
 }
