7547e402480e95010ab622c19cc765c2b71ef5bb
Arthur Rump <hello@arthurrump.com>
2020-04-17 12:47:36 +0200

    Fix submission page crashing on refresh
    Fixes #70

--- client/src/components/submission/SubmissionOverview.tsx
+++ client/src/components/submission/SubmissionOverview.tsx
@@ -3,6 +3,7 @@ import {Link, useHistory} from "react-router-dom";
 import {Button, Jumbotron} from "react-bootstrap";
 import {FiPlus, FiTrash, FiX} from "react-icons/all";
 
+import {Submission} from "../../../../models/api/Submission";
 import {ThreadState} from "../../../../models/enums/ThreadStateEnum";
 
 import {useSubmission, useFiles, useProjectComments, useRecentComments, useCourse, useCurrentUser} from "../../helpers/api/APIHooks";
@@ -18,6 +19,22 @@ import {Frame} from "../frame/Frame";
 import {Cached} from "../general/loading/Cached";
 import {ButtonMultistate} from "../input/button/ButtonMultistate";
 
+interface SubmissionDetailsProperties {
+	submission: Submission
+}
+function SubmissionDetails({submission}: SubmissionDetailsProperties) {
+	const course = useCourse(submission.references.courseID);
+	return <Cached cache={course}>
+		{course =>
+			<p>
+				Uploaded by <Link to={"/user/" + submission.user.ID}>{submission.user.name}</Link>, for <Link
+				to={"/course/" + course.ID}>{course.name}</Link>
+				<br/>
+				<small className="text-light">{TimeHelper.toDateTimeString(TimeHelper.fromString(submission.date))}</small>
+			</p>
+		}
+	</Cached>;
+}
 interface SubmissionOverviewProperties {
 	match: {
 		params: {
@@ -61,16 +78,7 @@ export function SubmissionOverview({match: {params: {submissionId}}}: Submission
 			<Frame title={submission.name} sidebar search={{course: submission.references.courseID, submission: submissionId}}>
 				<Jumbotron>
 					<h1>{submission.name}</h1>
-					<Cached cache={useCourse(submission.references.courseID)}>
-						{course =>
-							<p>
-								Uploaded by <Link to={"/user/" + submission.user.ID}>{submission.user.name}</Link>, for <Link
-								to={"/course/" + course.ID}>{course.name}</Link>
-								<br/>
-								<small className="text-light">{TimeHelper.toDateTimeString(TimeHelper.fromString(submission.date))}</small>
-							</p>
-						}
-					</Cached>
+					<SubmissionDetails submission={submission} />
 					<Button className="mb-2 mr-2"><Link to={submissionPath + "/share"}>Share</Link></Button>
 					<Button className="mb-2"><a href={`/api/submission/${submissionId}/archive`}>Download</a></Button>
 				</Jumbotron>
