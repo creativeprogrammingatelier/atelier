f225a46c5ec7cd43b22e200f3d9dc6120fefc73c
Arthur Rump <hello@arthurrump.com>
2020-05-02 20:00:45 +0200

    Make log level depend on environment

--- api/src/helpers/LoggingHelper.ts
+++ api/src/helpers/LoggingHelper.ts
@@ -1,6 +1,11 @@
 import pino from "pino";
+import {config} from "./ConfigurationHelper";
 
 /** Global instance of the logger */
 export const logger = pino({
-    name: "Server"
+    name: "Server",
+    level: 
+        config.env === "production"
+        ? "info"
+        : "debug"
 });
