aeb7c1f3539c0067e352c44f679b6c4c2a580374
Arthur Rump <hello@arthurrump.com>
2020-05-24 17:16:31 +0200

    Enable changing researchAllowed in account settings

--- client/src/components/settings/user/UserSettingsGeneral.tsx
+++ client/src/components/settings/user/UserSettingsGeneral.tsx
@@ -7,15 +7,20 @@ import {User} from "../../../../../models/api/User";
 import {useSubscription, useObservable} from "observable-hooks";
 import {useCurrentUser} from "../../../helpers/api/APIHooks";
 
-import {Label} from "../../general/Label";
+import {Area} from "../../general/Area";
 import {FeedbackError} from "../../feedback/FeedbackError";
 import {FeedbackContent} from "../../feedback/Feedback";
+import {CheckboxInput} from "../../input/CheckboxInput";
+import {LabeledInput} from "../../input/LabeledInput";
+import { FeedbackSuccess } from "../../feedback/FeedbackSuccess";
 
 export function UserSettingsGeneral() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [researchAllowed, setResearchAllowed] = useState(false);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false as FeedbackContent);
+    const [success, setSuccess] = useState(false as FeedbackContent);
     const user = useCurrentUser();
     
     const userObservable = useObservable(() => user.observable);
@@ -23,34 +28,46 @@ export function UserSettingsGeneral() {
         const user = (course as CacheItem<User>).value;
         setName(user?.name || "");
         setEmail(user?.email || "");
+        setResearchAllowed(user?.researchAllowed || false);
         setLoading(course.state !== CacheState.Loaded);
     });
     
     const handleUpdate = () => {
-        user.update({name, email})
+        setSuccess(false);
+        setError(false);
+        user.update({name, email, researchAllowed})
+            .then(_ => setSuccess("Your settings were successfully updated"))
             .catch(error => setError(`Failed to update course: ${error}`));
     };
     
     return <Form>
-        <Form.Label className="w-100">
-            <Label>Name</Label>
+        <LabeledInput label="Name">
             <Form.Control
                 type="text"
                 placeholder="Your name"
                 value={name}
                 onChange={(event: React.FormEvent<HTMLInputElement>) => setName((event.target as HTMLInputElement).value)}
             />
-        </Form.Label>
-        <Form.Label className="w-100">
-            <Label>Email</Label>
+        </LabeledInput>
+        <LabeledInput label="Email">
             <Form.Control
                 type="text"
                 placeholder="Your email"
                 value={email}
                 onChange={(event: React.FormEvent<HTMLInputElement>) => setEmail((event.target as HTMLInputElement).value)}
             />
-        </Form.Label>
+        </LabeledInput>
+        <LabeledInput label="Research">
+            <Area className="ml-2 my-2">
+                <CheckboxInput
+                    name="Submissions and comments I make on Atelier may be used for research purposes."
+                    value="researchAllowed"
+                    selected={researchAllowed}
+                    onChange={setResearchAllowed} />
+            </Area>
+        </LabeledInput>
         <FeedbackError close={setError}>{error}</FeedbackError>
+        <FeedbackSuccess close={setSuccess} timeout={3000}>{success}</FeedbackSuccess>
         <Button onClick={handleUpdate} disabled={loading}>Update</Button>
     </Form>;
 }
