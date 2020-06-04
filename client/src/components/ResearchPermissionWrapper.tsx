b9b18576cdf59e015588369ecea16e48a737c394
Arthur Rump <hello@arthurrump.com>
2020-06-04 14:37:30 +0200

    Disable consent by default

--- client/src/components/ResearchPermissionWrapper.tsx
+++ client/src/components/ResearchPermissionWrapper.tsx
@@ -27,7 +27,7 @@ export function ResearchPermissionWrapper({ children }: ParentalProperties) {
 export function ResearchPermissionInformation() {
     return <Fragment>
         <p>You have been invited to use a research prototype of Atelier. The aim of this platform is to improve the process of providing feedback to students on their programming assignments in a tutorial setting.</p>
-        <p>Your participation in this study is entirely voluntary. You can indicate consent for your data being used below. You can update this choice later by going to your account settings within Atelier. We plan to take a snapshot of all data on Atelier at the end on the tutorial period. We will take another snapshot at the end of the module.</p>
+        <p>Your participation in this study is entirely voluntary. You can indicate consent for your data being used below. You can update this choice later by going to your account settings within Atelier. We plan to take a snapshot of all data on Atelier at the end of the tutorial period. We will take another snapshot at the end of the module.</p>
         <p>If you consent, we will be using the programs that you upload; comments by students, student assistants, and automated tools; and an access count for research purposes. We will keep track of who consents to participating in this research, and who does not. All programs, comments, and usage statistics will be anonymized. Anonymized programs may be used for future research studies that may be similar to this study or may be completely different. Researchers will not contact you for additional permission to use this data. Any data shared with other researchers will not include any information that can directly identify you.</p>
         <p>If you do not consent, you can still use the fully-featured prototype, but your data will not be used in our research.</p>
         <p>We believe there are no known risks associated with participating in this research study, other than the standard risks that are associated with any online activities. Do not share or save any passwords or access tokens.</p>
@@ -37,7 +37,7 @@ export function ResearchPermissionInformation() {
 
 function ResearchPermissionOverlay() {
     const currentUser = useCurrentUser();
-    const [researchAllowed, setResearchAllowed] = useState(true);
+    const [researchAllowed, setResearchAllowed] = useState(false);
     const handleSubmit = () => currentUser.update({ researchAllowed });
 
     return <Overlay>
@@ -54,7 +54,7 @@ function ResearchPermissionOverlay() {
                     </CheckboxInput>
                 </Form.Group>
                 <Form.Group style={{ textAlign: "center" }}>
-                    <Button type="submit">Save</Button>
+                    <Button type="submit">{researchAllowed ? "Accept" : "Decline"}</Button>
                 </Form.Group>
             </Form>
         </Area>
