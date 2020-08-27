1c62606e0265ce1790f532561934e9deded2657d
Arthur Rump <hello@arthurrump.com>
2020-08-27 13:05:12 +0200

    Add basic feed filtering by type

--- client/src/components/feed/Feed.tsx
+++ client/src/components/feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FeedItem } from "../../../../models/api/FeedItem";
 import { FeedBlock } from "./FeedBlock";
 import { usePersonalFeed, useCourseFeed, LoadMore, Refresh } from "../../helpers/api/APIHooks";
@@ -6,6 +6,10 @@ import { Cached } from "../general/loading/Cached";
 import { IconType } from "react-icons";
 import { Button } from "react-bootstrap";
 import { FiFilter } from "react-icons/fi";
+import { CheckboxInput } from "../input/CheckboxInput";
+import { useObservable, pluckFirst } from "observable-hooks";
+import { map } from "rxjs/operators";
+import { combineLatest } from "rxjs";
 
 interface FeedButton {
     icon: IconType, 
@@ -20,13 +24,36 @@ interface FeedProperties {
 }
 
 function Feed({ feed, global, buttons }: FeedProperties) {
+    const [filtersExpanded, setFiltersExpanded] = useState(false);
+    const [filtered, setFiltered] = useState([ "submission", "mention", "commentThread", "comment" ]);
+    const filteredObservable = useObservable(pluckFirst, [filtered]);
+    const filteredFeedObservable = useObservable(() => 
+        combineLatest([feed.observable, filteredObservable]).pipe(
+            map(([col, filtered]) => 
+                "items" in col
+                ? { ...col, items: col.items.filter(item => filtered.includes(item.value.type)) } 
+                : col
+            ))
+    );
+    const filteredFeed = { ...feed, observable: filteredFeedObservable };
     return (
         <div className="m-3">
             <div className="feedButtons">
-                <Button><FiFilter /><span>Filter</span></Button>
-                {buttons.map(b => <Button onClick={b.onClick}>{b.icon({})}<span>{b.text}</span></Button>)}
+                <Button key="Filter" onClick={() => setFiltersExpanded(!filtersExpanded)}>
+                    <FiFilter fill={filtered.length !== 4 ? "#ffffff" : "none"} /><span>Filter</span>
+                </Button>
+                {buttons.map(b => <Button key={b.text} onClick={b.onClick}>{b.icon({})}<span>{b.text}</span></Button>)}
             </div>
-            <Cached cache={feed} extractDate={item => new Date(item.timestamp)}>
+            {
+                filtersExpanded &&
+                <div className="feedFilters">
+                    <FilterBox tag="submission" name="Submissions" filtered={filtered} setFiltered={setFiltered} />
+                    <FilterBox tag="mention" name="Mentions" filtered={filtered} setFiltered={setFiltered} />
+                    <FilterBox tag="commentThread" name="New comments" filtered={filtered} setFiltered={setFiltered} />
+                    <FilterBox tag="comment" name="Replies" filtered={filtered} setFiltered={setFiltered} />
+                </div>
+            }
+            <Cached cache={filteredFeed} extractDate={item => new Date(item.timestamp)}>
                 {item => <FeedBlock key={item.ID} data={item} global={global} />}
             </Cached>
         </div>
@@ -43,4 +70,25 @@ interface CourseFeedProperties { courseID: string, buttons?: FeedButton[] }
 export function CourseFeed({ courseID, buttons = [] }: CourseFeedProperties) {
     const feed = useCourseFeed(courseID);
     return (<Feed feed={feed} buttons={buttons} global={false} />);
+}
+
+interface FilterBoxProperties {
+    tag: string,
+    name: string,
+    filtered: string[],
+    setFiltered: (update: (filtered: string[]) => string[]) => void
+}
+function FilterBox({ tag, name, filtered, setFiltered }: FilterBoxProperties) {
+    return (
+        <CheckboxInput
+            children={name}
+            key={tag}
+            value={tag}
+            selected={filtered.includes(tag)}
+            onChange={state => {
+                state 
+                ? setFiltered(filtered => filtered.concat(tag))
+                : setFiltered(filtered => filtered.filter(f => f !== tag));
+            }} />
+    );
 }
